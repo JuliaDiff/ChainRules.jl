b3f66e3bdcf0142b9dd3ea0f46c06fdea6b2d10f
Jarrett Revels <jarrettrevels@gmail.com>
2019-04-10 00:37:38 -0400

    fix typo

--- docs/make.jl
+++ docs/make.jl
@@ -6,6 +6,6 @@ makedocs(modules=[ChainRules],
          authors="Jarrett Revels and other contributors",
          pages=["Introduction" => "index.md",
                 "Getting Started" => "getting_started.md",
-                "ChainRules API Documentation" => "api.md"]])
+                "ChainRules API Documentation" => "api.md"])
 
 deploydocs(repo="github.com/JuliaDiff/ChainRules.jl.git")
