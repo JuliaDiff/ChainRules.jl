8d9fe3660b0bb080c1a12e51b198fc27d0b3785c
Alex Arslan <ararslan@comcast.net>
2019-06-05 20:38:40 +0200

    Add rrules for hcat and vcat

--- src/rules/array.jl
+++ src/rules/array.jl
@@ -10,3 +10,35 @@ function rrule(::typeof(reshape), A::AbstractArray, dims::Int...)
     Y, (rule, _) = rrule(reshape, A, dims)
     return Y, (rule, fill(DNERule(), length(dims))...)
 end
+
+#####
+##### `hcat` (🐈)
+#####
+
+function rrule(::typeof(hcat), A::AbstractArray, Bs::AbstractArray...)
+    Y = hcat(A, Bs...)
+    Xs = (A, Bs...)
+    rules = ntuple(length(Bs) + 1) do i
+        l = mapreduce(j->size(Xs[j], 2), Base.add_sum, 1:i-1; init=0)
+        u = l + size(Xs[i], 2)
+        dim = u > l + 1 ? (l+1:u) : u
+        Rule(Ȳ->copy(selectdim(Ȳ, 2, dim)))
+    end
+    return Y, rules
+end
+
+#####
+##### `vcat`
+#####
+
+function rrule(::typeof(vcat), A::AbstractArray, Bs::AbstractArray...)
+    Y = vcat(A, Bs...)
+    n = size(A, 1)
+    ∂A = Rule(Ȳ->copy(selectdim(Ȳ, 1, 1:n)))
+    ∂Bs = ntuple(length(Bs)) do i
+        l = n + mapreduce(j->size(Bs[j], 1), Base.add_sum, 1:i-1; init=0)
+        u = l + size(Bs[i], 1)
+        Rule(Ȳ->copy(selectdim(Ȳ, 1, l+1:u)))
+    end
+    return Y, (∂A, ∂Bs...)
+end
--- test/rules/array.jl
+++ test/rules/array.jl
@@ -16,3 +16,29 @@
     Ā = dA(Ȳ)
     @test Ā == reshape(Ȳ, 5, 4)
 end
+
+@testset "hcat" begin
+    rng = MersenneTwister(2)
+    A = randn(rng, 3, 2)
+    B = randn(rng, 3)
+    C = randn(rng, 3, 3)
+    H, (dA, dB, dC) = rrule(hcat, A, B, C)
+    @test H == hcat(A, B, C)
+    H̄ = randn(rng, 3, 6)
+    @test dA(H̄) ≈ view(H̄, :, 1:2)
+    @test dB(H̄) ≈ view(H̄, :, 3)
+    @test dC(H̄) ≈ view(H̄, :, 4:6)
+end
+
+@testset "vcat" begin
+    rng = MersenneTwister(3)
+    A = randn(rng, 2, 4)
+    B = randn(rng, 1, 4)
+    C = randn(rng, 3, 4)
+    V, (dA, dB, dC) = rrule(vcat, A, B, C)
+    @test V == vcat(A, B, C)
+    V̄ = randn(rng, 6, 4)
+    @test dA(V̄) ≈ view(V̄, 1:2, :)
+    @test dB(V̄) ≈ view(V̄, 3:3, :)
+    @test dC(V̄) ≈ view(V̄, 4:6, :)
+end
