99dc361cfecfee72b131e89a7ad22a711e8fdd07
Alex Arslan <ararslan@comcast.net>
2019-06-19 12:22:53 -0700

    Ensure scalar_rule rules are Number typed
    Currently, `@scalar_rule` generates `rrule` methods with no type
    constraints. This poses a problem for rules with different definitions
    that have not yet been implemented. For example, the rule for matrix
    exponential is quite different from that for scalar exponential, but in
    the absence of an `rrule(::typeof(exp), ::AbstractMatrix)` method, the
    incorrect fallback generated by `@scalar_rule` is used.
    
    To solve this, `@scalar_rule` now allows type constaints, e.g.
    `@scalar_rule(f(x::Complex), g(x))`, and it adds explicit `::Number`
    constraints to the generated methods if no such constraints are
    provided.

--- test/rules.jl
+++ test/rules.jl
@@ -1,13 +1,25 @@
 cool(x) = x + 1
 cool(x, y) = x + y + 1
 
+_second(t) = Base.tuple_type_head(Base.tuple_type_tail(t))
+
 @testset "rules" begin
     @testset "frule and rrule" begin
         @test frule(cool, 1) === nothing
         @test frule(cool, 1; iscool=true) === nothing
         @test rrule(cool, 1) === nothing
         @test rrule(cool, 1; iscool=true) === nothing
+
         ChainRules.@scalar_rule(Main.cool(x), one(x))
+        @test hasmethod(rrule, Tuple{typeof(cool),Number})
+        ChainRules.@scalar_rule(Main.cool(x::String), "wow such dfdx")
+        @test hasmethod(rrule, Tuple{typeof(cool),String})
+        # Ensure those are the *only* methods that have been defined
+        cool_methods = Set(m.sig for m in methods(rrule) if _second(m.sig) == typeof(cool))
+        only_methods = Set([Tuple{typeof(rrule),typeof(cool),Number},
+                            Tuple{typeof(rrule),typeof(cool),String}])
+        @test cool_methods == only_methods
+
         frx, fr = frule(cool, 1)
         @test frx == 2
         @test fr(1) == 1
