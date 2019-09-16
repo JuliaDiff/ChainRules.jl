fc054d9a9f0f6077dc0ffa3fdd94f0f95373cd46
Lyndon White <lyndon.white@invenialabs.co.uk>
2019-09-16 10:52:55 +0100

    PR preview docs

--- docs/make.jl
+++ docs/make.jl
@@ -14,4 +14,20 @@ makedocs(
     ],
 )
 
-deploydocs(repo="github.com/JuliaDiff/ChainRules.jl.git")
+const repo=="github.com/JuliaDiff/ChainRules.jl.git"
+if get(ENV, "TRAVIS_PULL_REQUEST", "false") == "false"
+    # Normal Case
+    deploydocs(repo=repo)
+else
+    @info "Deploying review docs for PR #$PR"
+    # Overwrite Documenter's function for generating the versions.js file
+    foreach(Base.delete_method, methods(Documenter.Writers.HTMLWriter.generate_version_file))
+    Documenter.Writers.HTMLWriter.generate_version_file(_, _) = nothing
+    # Overwrite necessary environment variables to trick Documenter to deploy
+    ENV["TRAVIS_PULL_REQUEST"] = "false"
+    ENV["TRAVIS_BRANCH"] = "master"
+    deploydocs(
+        devurl="preview-PR$(PR)",
+        repo=repo,
+    )
+end
