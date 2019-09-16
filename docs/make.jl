eb8930dcdf7187becd6c0ca388fa2f8ba2fb3593
Lyndon White <oxinabox@ucc.asn.au>
2019-09-16 13:45:20 +0100

    add link to remind to clean up this code once less evil way is possible

--- docs/make.jl
+++ docs/make.jl
@@ -20,12 +20,15 @@ if get(ENV, "TRAVIS_PULL_REQUEST", "false") == "false"
     deploydocs(repo=repo)
 else
     @info "Deploying review docs for PR #$PR"
+    # TODO: remove most of this once https://github.com/JuliaDocs/Documenter.jl/issues/1131 is resolved
+    
     # Overwrite Documenter's function for generating the versions.js file
     foreach(Base.delete_method, methods(Documenter.Writers.HTMLWriter.generate_version_file))
     Documenter.Writers.HTMLWriter.generate_version_file(_, _) = nothing
     # Overwrite necessary environment variables to trick Documenter to deploy
     ENV["TRAVIS_PULL_REQUEST"] = "false"
     ENV["TRAVIS_BRANCH"] = "master"
+    
     deploydocs(
         devurl="preview-PR$(PR)",
         repo=repo,
