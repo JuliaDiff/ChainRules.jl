1b67271064f444cb91d545593b3ff378b9199ffe
Takafumi Arakaki <aka.tkf@gmail.com>
2019-09-19 21:12:17 -0700

    Add rrule for Diagonal * AbstractVector

--- src/rulesets/LinearAlgebra/structured.jl
+++ src/rulesets/LinearAlgebra/structured.jl
@@ -19,6 +19,13 @@ function rrule(::typeof(diag), A::AbstractMatrix)
     return diag(A), diag_pullback
 end
 
+function rrule(::typeof(*), D::Diagonal{<:Real}, V::AbstractVector{<:Real})
+    function times_pullback(Ȳ)
+        return (NO_FIELDS, @thunk(Diagonal(Ȳ .* V)), @thunk(D.diag .* Ȳ))
+    end
+    return D.diag .* V, times_pullback
+end
+
 #####
 ##### `Symmetric`
 #####
