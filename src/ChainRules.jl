40382c6b7f6a7e3c13cd2ed65c3dfe24a54c69fa
Yingbo Ma <mayingbo5@gmail.com>
2019-12-24 15:57:37 -0500

    Fix NaNMath and SpecialFunctions warnings
    ```julia
    julia> using NaNMath, SpecialFunctions
    ┌ Warning: Error requiring NaNMath from ChainRules:
    │ LoadError: UndefVarError: SpecialFunctions not defined
    │ Stacktrace:
    │  [1] include at ./boot.jl:328 [inlined]
    │  [2] include_relative(::Module, ::String) at ./loading.jl:1105
    │  [3] include at ./Base.jl:31 [inlined]
    │  [4] include(::String) at /home/scheme/.julia/dev/ChainRules/src/ChainRules.jl:1
    │  [5] top-level scope at /home/scheme/.julia/dev/ChainRules/src/ChainRules.jl:45
    │  [6] eval at ./boot.jl:330 [inlined]
    │  [7] eval at /home/scheme/.julia/dev/ChainRules/src/ChainRules.jl:1 [inlined]
    │  [8] (::ChainRules.var"#867#873")() at /home/scheme/.julia/packages/Requires/9Jse8/src/require.jl:67
    │  [9] err(::ChainRules.var"#867#873", ::Module, ::String) at /home/scheme/.julia/packages/Requires/9Jse8/src/require.jl:38
    │  [10] #866 at /home/scheme/.julia/packages/Requires/9Jse8/src/require.jl:66 [inlined]
    │  [11] withpath(::ChainRules.var"#866#872", ::String) at /home/scheme/.julia/packages/Requires/9Jse8/src/require.jl:28
    │  [12] (::ChainRules.var"#865#871")() at /home/scheme/.julia/packages/Requires/9Jse8/src/require.jl:65
    │  [13] #invokelatest#1 at ./essentials.jl:709 [inlined]
    │  [14] invokelatest at ./essentials.jl:708 [inlined]
    │  [15] #3 at /home/scheme/.julia/packages/Requires/9Jse8/src/require.jl:19 [inlined]
    │  [16] iterate at ./generator.jl:47 [inlined]
    │  [17] _collect(::Array{Function,1}, ::Base.Generator{Array{Function,1},Requires.var"#3#4"}, ::Base.EltypeUnknown, ::Base.HasShape{1}) at ./array.jl:635
    │  [18] map at ./array.jl:564 [inlined]
    │  [19] loadpkg(::Base.PkgId) at /home/scheme/.julia/packages/Requires/9Jse8/src/require.jl:19
    │  [20] #invokelatest#1 at ./essentials.jl:709 [inlined]
    │  [21] invokelatest at ./essentials.jl:708 [inlined]
    │  [22] require(::Base.PkgId) at ./loading.jl:925
    │  [23] require(::Module, ::Symbol) at ./loading.jl:917
    │  [24] eval(::Module, ::Any) at ./boot.jl:330
    │  [25] eval_user_input(::Any, ::REPL.REPLBackend) at /buildworker/worker/package_linux64/build/usr/share/julia/stdlib/v1.3/REPL/src/REPL.jl:86
    │  [26] run_backend(::REPL.REPLBackend) at /home/scheme/.julia/packages/Revise/S7mrl/src/Revise.jl:1057
    │  [27] (::Revise.var"#85#87"{REPL.REPLBackend})() at ./task.jl:333
    │ in expression starting at /home/scheme/.julia/dev/ChainRules/src/rulesets/packages/NaNMath.jl:4
    └ @ Requires ~/.julia/packages/Requires/9Jse8/src/require.jl:40
    ```

--- src/ChainRules.jl
+++ src/ChainRules.jl
@@ -42,13 +42,103 @@ include("rulesets/LinearAlgebra/factorization.jl")
 # So we define them here for them.
 function __init__()
     @require NaNMath="77ba4419-2d1f-58cd-9bb1-8ffee604a2e3" begin
-        include("rulesets/packages/NaNMath.jl")
-        using .NaNMathGlue
+        @scalar_rule(NaNMath.sin(x), NaNMath.cos(x))
+        @scalar_rule(NaNMath.cos(x), -NaNMath.sin(x))
+        @scalar_rule(NaNMath.asin(x), inv(NaNMath.sqrt(1 - NaNMath.pow(x, 2))))
+        @scalar_rule(NaNMath.acos(x), -inv(NaNMath.sqrt(1 - NaNMath.pow(x, 2))))
+        @scalar_rule(NaNMath.acosh(x), inv(NaNMath.sqrt(NaNMath.pow(x, 2) - 1)))
+        @scalar_rule(NaNMath.atanh(x), inv(1 - NaNMath.pow(x, 2)))
+        @scalar_rule(NaNMath.log(x), inv(x))
+        @scalar_rule(NaNMath.log2(x), inv(x) / NaNMath.log(oftype(x, 2)))
+        @scalar_rule(NaNMath.log10(x), inv(x) / NaNMath.log(oftype(x, 10)))
+        @scalar_rule(NaNMath.log1p(x), inv(x + 1))
+        @scalar_rule(NaNMath.lgamma(x), SpecialFunctions.digamma(x))
+        @scalar_rule(NaNMath.sqrt(x), inv(2 * Ω))
+        @scalar_rule(NaNMath.pow(x, y), (y * NaNMath.pow(x, y - 1), Ω * NaNMath.log(x)))
+        @scalar_rule(NaNMath.max(x, y),
+                     (ifelse((y > x) | (signbit(y) < signbit(x)), ifelse(isnan(y), One(), Zero()), ifelse(isnan(x), Zero(), One())),
+                      ifelse((y > x) | (signbit(y) < signbit(x)), ifelse(isnan(y), Zero(), One()), ifelse(isnan(x), One(), Zero()))))
+        @scalar_rule(NaNMath.min(x, y),
+                     (ifelse((y < x) | (signbit(y) > signbit(x)), ifelse(isnan(y), One(), Zero()), ifelse(isnan(x), Zero(), One())),
+                      ifelse((y < x) | (signbit(y) > signbit(x)), ifelse(isnan(y), Zero(), One()), ifelse(isnan(x), One(), Zero()))))
     end
 
     @require SpecialFunctions="276daf66-3868-5448-9aa4-cd146d93841b" begin
-        include("rulesets/packages/SpecialFunctions.jl")
-        using .SpecialFunctionsGlue
+        @scalar_rule(SpecialFunctions.erf(x), (2 / sqrt(π)) * exp(-x * x))
+        @scalar_rule(SpecialFunctions.erfc(x), -(2 / sqrt(π)) * exp(-x * x))
+        @scalar_rule(SpecialFunctions.erfi(x), (2 / sqrt(π)) * exp(x * x))
+        @scalar_rule(SpecialFunctions.digamma(x), SpecialFunctions.trigamma(x))
+        @scalar_rule(SpecialFunctions.trigamma(x), SpecialFunctions.polygamma(2, x))
+        @scalar_rule(SpecialFunctions.airyai(x), SpecialFunctions.airyaiprime(x))
+        @scalar_rule(SpecialFunctions.airyaiprime(x), x * SpecialFunctions.airyai(x))
+        @scalar_rule(SpecialFunctions.airybi(x), SpecialFunctions.airybiprime(x))
+        @scalar_rule(SpecialFunctions.airybiprime(x), x * SpecialFunctions.airybi(x))
+        @scalar_rule(SpecialFunctions.besselj0(x), -SpecialFunctions.besselj1(x))
+        @scalar_rule(SpecialFunctions.bessely0(x), -SpecialFunctions.bessely1(x))
+        @scalar_rule(SpecialFunctions.invdigamma(x), inv(SpecialFunctions.trigamma(SpecialFunctions.invdigamma(x))))
+        @scalar_rule(SpecialFunctions.besselj1(x), (SpecialFunctions.besselj0(x) - SpecialFunctions.besselj(2, x)) / 2)
+        @scalar_rule(SpecialFunctions.bessely1(x), (SpecialFunctions.bessely0(x) - SpecialFunctions.bessely(2, x)) / 2)
+        @scalar_rule(SpecialFunctions.gamma(x), Ω * SpecialFunctions.digamma(x))
+        @scalar_rule(SpecialFunctions.erfinv(x), (sqrt(π) / 2) * exp(Ω^2))
+        @scalar_rule(SpecialFunctions.erfcinv(x), -(sqrt(π) / 2) * exp(Ω^2))
+        @scalar_rule(SpecialFunctions.erfcx(x), (2 * x * Ω) - (2 / sqrt(π)))
+        @scalar_rule(SpecialFunctions.dawson(x), 1 - (2 * x * Ω))
+
+        # binary
+        @scalar_rule(SpecialFunctions.besselj(ν, x),
+                     (NaN,
+                      (SpecialFunctions.besselj(ν - 1, x) -
+                       SpecialFunctions.besselj(ν + 1, x)) / 2))
+
+        @scalar_rule(SpecialFunctions.besseli(ν, x),
+                     (NaN,
+                      (SpecialFunctions.besseli(ν - 1, x) +
+                       SpecialFunctions.besseli(ν + 1, x)) / 2))
+        @scalar_rule(SpecialFunctions.bessely(ν, x),
+                     (NaN,
+                      (SpecialFunctions.bessely(ν - 1, x) -
+                       SpecialFunctions.bessely(ν + 1, x)) / 2))
+
+        @scalar_rule(SpecialFunctions.besselk(ν, x),
+                     (NaN,
+                      -(SpecialFunctions.besselk(ν - 1, x) +
+                        SpecialFunctions.besselk(ν + 1, x)) / 2))
+
+        @scalar_rule(SpecialFunctions.hankelh1(ν, x),
+                     (NaN,
+                      (SpecialFunctions.hankelh1(ν - 1, x) -
+                       SpecialFunctions.hankelh1(ν + 1, x)) / 2))
+        @scalar_rule(SpecialFunctions.hankelh2(ν, x),
+                     (NaN,
+                      (SpecialFunctions.hankelh2(ν - 1, x) -
+                       SpecialFunctions.hankelh2(ν + 1, x)) / 2))
+
+        @scalar_rule(SpecialFunctions.polygamma(m, x),
+                     (NaN, SpecialFunctions.polygamma(m + 1, x)))
+
+        # todo: setup for common expr
+        @scalar_rule(SpecialFunctions.beta(a, b),
+                     (Ω*(SpecialFunctions.digamma(a) - SpecialFunctions.digamma(a + b)),
+                      Ω*(SpecialFunctions.digamma(b) - SpecialFunctions.digamma(a + b))))
+
+        @scalar_rule(SpecialFunctions.lbeta(a, b),
+                     (SpecialFunctions.digamma(a) - SpecialFunctions.digamma(a + b),
+                      SpecialFunctions.digamma(b) - SpecialFunctions.digamma(a + b)))
+
+        # Changes between SpecialFunctions 0.7 and 0.8
+        if isdefined(SpecialFunctions, :lgamma)
+            # actually is the absolute value of the logorithm of gamma
+            @scalar_rule(SpecialFunctions.lgamma(x), SpecialFunctions.digamma(x))
+        end
+
+        if isdefined(SpecialFunctions, :logabsgamma)
+            # actually is the absolute value of the logorithm of gamma, paired with sign gamma
+            @scalar_rule(SpecialFunctions.logabsgamma(x), SpecialFunctions.digamma(x), Zero())
+        end
+
+        if isdefined(SpecialFunctions, :loggamma)
+            @scalar_rule(SpecialFunctions.loggamma(x), SpecialFunctions.digamma(x))
+        end
     end
 end
 
