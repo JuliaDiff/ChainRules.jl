4366b26a9c622bad9e84e4e654cc164313098d12
Seth Axen <seth.axen@gmail.com>
2020-05-17 16:24:22 -0700

    Add eigen and eigvals tests

--- test/rulesets/LinearAlgebra/structured.jl
+++ test/rulesets/LinearAlgebra/structured.jl
@@ -14,7 +14,7 @@
         comp = Composite{typeof(res)}(; diag=10*res.diag)  # this is the structure of Diagonal
         @test pb(comp) == (NO_FIELDS, [10, 40])
     end
-    
+
     @testset "::Diagonal * ::AbstractVector" begin
         N = 3
         rrule_test(
@@ -44,5 +44,74 @@
     @testset "$T" for T in (UpperTriangular, LowerTriangular)
         n = 5
         rrule_test(T, T(randn(n, n)), (randn(n, n), randn(n, n)))
+    end
+
+    @testset "eigen(::$T{<:Real})" for T in (Symmetric, Hermitian)
+        n = 5
+        @testset "frule" begin
+            @testset for uplo in (:L, :U)
+                A, ΔA = T(randn(n, n), uplo), T(randn(n, n), uplo)
+                F = eigen(A)
+                F_ad, ∂F_ad = frule((Zero(), ΔA), eigen, A)
+                @test F_ad == F
+                ∂F_ad = unthunk(∂F_ad)
+                @test ∂F_ad isa Composite{typeof(F)}
+                @test ∂F_ad.values ≈ jvp(_fdm, A -> eigen(T(A, uplo)).values, (A.data, ΔA.data))
+                # TODO: adopt a deterministic sign convention to stabilize the FD estimate
+                @test ∂F_ad.vectors ≈ jvp(_fdm, A -> eigen(T(A, uplo)).vectors, (A.data, ΔA.data))
+            end
+        end
+
+        @testset "rrule" begin
+            @testset for uplo in (:L, :U)
+                A, ΔU, Δλ = T(randn(n, n), uplo), randn(n, n), randn(n)
+                F = eigen(A)
+                ΔFall = Composite{typeof(F)}(values = Δλ, vectors = ΔU)
+                # NOTE: how can we test pulling back both Δλ and ΔU?
+                # TODO: adopt a deterministic sign convention to stabilize the FD estimate
+                @testset for p in (:values, :vectors)
+                    F_ad, back = rrule(eigen, A)
+                    @test F_ad == F
+                    ΔFp = getproperty(ΔFall, p)
+                    ΔF = Composite{typeof(F)}(; p => ΔFp)
+                    ∂self, ∂A = back(ΔF)
+                    @test ∂self === NO_FIELDS
+                    ∂A = unthunk(∂A)
+                    @test ∂A isa typeof(A)
+                    @test ∂A.uplo == A.uplo
+                    @test ∂A.data ≈ only(j′vp(_fdm, A -> getproperty(eigen(T(A, uplo)), p), ΔFp, A.data))
+                end
+            end
+        end
+    end
+
+    @testset "eigvals(::$T{<:Real})" for T in (Symmetric, Hermitian)
+        n = 5
+        @testset "frule" begin
+            @testset for uplo in (:L, :U)
+                A, ΔA = T(randn(n, n), uplo), T(randn(n, n), uplo)
+                λ = eigvals(A)
+                λ_ad, ∂λ_ad = frule((Zero(), ΔA), eigvals, A)
+                @test λ_ad ≈ λ # inexact because frule uses eigen not eigvals
+                ∂λ_ad = unthunk(∂λ_ad)
+                @test ∂λ_ad isa typeof(λ)
+                @test ∂λ_ad ≈ jvp(_fdm, A -> eigvals(T(A, uplo)), (A.data, ΔA.data))
+            end
+        end
+
+        @testset "rrule" begin
+            @testset for uplo in (:L, :U)
+                A, Δλ = T(randn(n, n), uplo), randn(n)
+                λ = eigvals(A)
+                λ_ad, back = rrule(eigvals, A)
+                @test λ_ad ≈ λ # inexact because frule uses eigen not eigvals
+                ∂self, ∂A = back(Δλ)
+                @test ∂self === NO_FIELDS
+                ∂A = unthunk(∂A)
+                @test ∂A isa typeof(A)
+                @test ∂A.uplo == A.uplo
+                @test ∂A.data ≈ only(j′vp(_fdm, A -> eigvals(T(A, uplo)), Δλ, A.data))
+            end
+        end
     end
 end
