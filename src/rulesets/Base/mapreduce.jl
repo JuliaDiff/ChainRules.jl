74eb89099752ef1211b8faa79039f17e545e91b4
Seth Axen <seth.axen@gmail.com>
2020-06-28 20:18:04 -0700

    Release type constraints

--- src/rulesets/Base/mapreduce.jl
+++ src/rulesets/Base/mapreduce.jl
@@ -6,7 +6,7 @@ function frule((_, ẋ), ::typeof(sum), x)
     return sum(x), sum(ẋ)
 end
 
-function rrule(::typeof(sum), x::AbstractArray{<:Real}; dims=:)
+function rrule(::typeof(sum), x::AbstractArray{T}; dims=:) where {T<:Number}
     y = sum(sum, x; dims=dims)
     function sum_pullback(ȳ)
         # broadcasting the two works out the size no-matter `dims`
@@ -18,7 +18,12 @@ function rrule(::typeof(sum), x::AbstractArray{<:Real}; dims=:)
     return y, sum_pullback
 end
 
-function rrule(::typeof(sum), ::typeof(abs2), x::AbstractArray{<:Real}; dims=:)
+function rrule(
+    ::typeof(sum),
+    ::typeof(abs2),
+    x::AbstractArray{T};
+    dims=:,
+) where {T<:Union{Real,Complex}}
     y = sum(abs2, x; dims=dims)
     function sum_abs2_pullback(ȳ)
         return (NO_FIELDS, DoesNotExist(), @thunk(2ȳ .* x))
