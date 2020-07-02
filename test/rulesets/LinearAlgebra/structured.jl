6f79df03fef0807147e6d6ce8de098d0f84f1948
Seth Axen <seth.axen@gmail.com>
2020-07-02 11:20:22 -0700

    Use more informative type names

--- test/rulesets/LinearAlgebra/structured.jl
+++ test/rulesets/LinearAlgebra/structured.jl
@@ -78,7 +78,8 @@
             end
         end
     end
-    @testset "$(TA)(::AbstractMatrix{$T}, '$(uplo)')" for TA in (Symmetric, Hermitian),
+    @testset "$(SymHerm)(::AbstractMatrix{$T}, :$(uplo))" for
+        SymHerm in (Symmetric, Hermitian),
         T in (Float64, ComplexF64),
         uplo in (:U, :L)
 
@@ -87,10 +88,10 @@
             x = randn(T, N, N)
             Δx = randn(T, N, N)
             # can't use frule_test here because it doesn't yet ignore nothing tangents
-            Ω = TA(x, uplo)
-            Ω_ad, ∂Ω_ad = frule((Zero(), Δx, Zero()), TA, x, uplo)
+            Ω = SymHerm(x, uplo)
+            Ω_ad, ∂Ω_ad = frule((Zero(), Δx, Zero()), SymHerm, x, uplo)
             @test Ω_ad == Ω
-            ∂Ω_fd = jvp(_fdm, z -> TA(z, uplo), (x, Δx))
+            ∂Ω_fd = jvp(_fdm, z -> SymHerm(z, uplo), (x, Δx))
             @test ∂Ω_ad ≈ ∂Ω_fd
         end
         @testset "rrule" begin
@@ -98,26 +99,26 @@
             ∂x = randn(T, N, N)
             ΔΩ = randn(T, N, N)
             @testset "back(::$MT)" for MT in (Matrix, LowerTriangular, UpperTriangular)
-                rrule_test(TA, MT(ΔΩ), (x, ∂x), (uplo, nothing))
+                rrule_test(SymHerm, MT(ΔΩ), (x, ∂x), (uplo, nothing))
             end
             @testset "back(::Diagonal)" begin
-                rrule_test(TA, Diagonal(ΔΩ), (x, Diagonal(∂x)), (uplo, nothing))
+                rrule_test(SymHerm, Diagonal(ΔΩ), (x, Diagonal(∂x)), (uplo, nothing))
             end
         end
     end
-    @testset "$(TM)(::$(TA){$T}) with uplo='$uplo'" for TM in (Matrix, Array),
-        TA in (Symmetric, Hermitian),
+    @testset "$(f)(::$(SymHerm){$T}) with uplo=:$uplo" for f in (Matrix, Array),
+        SymHerm in (Symmetric, Hermitian),
         T in (Float64, ComplexF64),
         uplo in (:U, :L)
 
         N = 3
-        x = TA(randn(T, N, N), uplo)
+        x = SymHerm(randn(T, N, N), uplo)
         Δx = randn(T, N, N)
-        ∂x = TA(randn(T, N, N), uplo)
-        ΔΩ = TM(TA(randn(T, N, N), uplo))
-        frule_test(TM, (x, Δx))
-        frule_test(TM, (x, TA(Δx, uplo)))
-        rrule_test(TM, ΔΩ, (x, ∂x))
+        ∂x = SymHerm(randn(T, N, N), uplo)
+        ΔΩ = f(SymHerm(randn(T, N, N), uplo))
+        frule_test(f, (x, Δx))
+        frule_test(f, (x, SymHerm(Δx, uplo)))
+        rrule_test(f, ΔΩ, (x, ∂x))
     end
     @testset "$f" for f in (Adjoint, adjoint, Transpose, transpose)
         n = 5
