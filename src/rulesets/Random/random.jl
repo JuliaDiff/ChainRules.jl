af416f4838c7da632ad2eae1762bec9b8239268d
Nick Robinson <nicholas.robinson@invenialabs.co.uk>
2020-07-06 12:13:30 +0100

    Fixup naming convention `*_rrule` -> `*_pullback`

--- src/rulesets/Random/random.jl
+++ src/rulesets/Random/random.jl
@@ -1,8 +1,8 @@
 frule(Δargs, ::Type{MersenneTwister}, args...) = MersenneTwister(args...), Zero()
 
 function rrule(::Type{MersenneTwister}, args...)
-    function MersenneTwister_rrule(ΔΩ)
+    function MersenneTwister_pullback(ΔΩ)
         return (NO_FIELDS, map(_ -> Zero(), args)...)
     end
-    return MersenneTwister(args...), MersenneTwister_rrule
+    return MersenneTwister(args...), MersenneTwister_pullback
 end
