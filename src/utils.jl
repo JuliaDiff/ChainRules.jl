cff851f9e532a303ca05c1369b10af2c61463826
Gaurav Dhingra <gaurav@digitalaristotle.ai>
2020-10-01 17:09:59 +0530

    fix the rrule, and frule

--- src/utils.jl
+++ src/utils.jl
@@ -1,9 +1,9 @@
-function frule((_, ḟ), ::typeof(ignore), f)
-    return f(), nothing
+function frule((_, ḟ), ::typeof(ignore), f, args...; kwargs...)
+    return f(args...; kwargs...), nothing
 end
 
-function rrule(::typeof(ignore), f)
-    y = f()
+function rrule(::typeof(ignore), f, args...; kwargs...)
+    y = f(args...; kwargs...)
     function ignore_pullback(ȳ)
         return (NO_FIELDS, nothing)
     end
