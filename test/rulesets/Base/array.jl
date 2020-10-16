ba9a148368396ac1adb9901719a3abaa7ae0cd2c
Lyndon White <oxinabox@ucc.asn.au>
2020-10-16 16:57:20 +0100

    Add tests of overlapping indexes

--- test/rulesets/Base/array.jl
+++ test/rulesets/Base/array.jl
@@ -81,4 +81,8 @@ end
     x̄ = [1.4 2.5 3.7; 10.5 20.1 30.2]
     rrule_test(getindex, 2.3, (x, x̄), (2, nothing))
     rrule_test(getindex, 2.3, (x, x̄), (2, nothing), (1, nothing))
+    
+    # overlapping indexes
+    rrule_test(getindex, 2.3, (x, x̄), (2, nothing), (2, nothing))
+    rrule_test(getindex, 2.3, (x, x̄), (2, nothing), (1, nothing), (2, nothing))
 end
