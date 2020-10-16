b0e2c729813019a8017e48efbdd97c7862ac208f
Lyndon White <lyndon.white@invenialabs.co.uk>
2020-10-16 21:44:28 +0100

    WIP generalize getindex to work on anye element type

--- test/rulesets/Base/indexing.jl
+++ test/rulesets/Base/indexing.jl
@@ -1,4 +1,4 @@
-@testset "getindex" begin
+@testset "indexing.jl" begin
     @testset "getindex(::Matrix{<:Number},...)" begin
         x = [1.0 2.0 3.0; 10.0 20.0 30.0]
         x̄ = [1.4 2.5 3.7; 10.5 20.1 30.2]
@@ -53,5 +53,33 @@
                 getindex, [2.3 3.1; 4.1 5.1], (x, x̄), ([2,2], nothing), ([3,3], nothing)
             )
         end
+    end
+
+    @testset "getindex(::Matrix{Not a Number},...)" begin
+        rrule_test(  # Vector of Vectors
+            getindex, [[2.3,],], ([[1.0,],[2.0]], [[2.1,],[310]]), (2, nothing)
+        )
+        rrule_test( # Vector of Vectors of different sizes
+            getindex, [[2.3, 1.1],], ([[1.0,],[2.0,3.0]], [[2.1,],[3.2, 2.1]]), (2, nothing)
+        )
+
+        # shorthand constructor for Composite with primal matching backing
+        Compo(x...) = Composite{typeof(x)}(x...)
+        rrule_test( # Vector of Tuples
+            getindex,
+            [Compo(2.5, 4.1),],
+            (
+                [(1.1, 2.3), (1.3, 2.5)], [Compo(2.5, 4.1), Compo(2.5, 4.1)]
+            ),
+            (2, nothing)
+        )
+        rrule_test( # Vector of Tuples of different sizes
+            getindex,
+            [Compo(2.5, 4.1),],
+            (
+                [(1.1, 2.3), (1.3, 2.5)], [Compo(2.5, 4.1), Compo(2.5, 4.1)]
+            ),
+            (2, nothing)
+        )
     end
 end
