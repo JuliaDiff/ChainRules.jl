50c6c607412c47247ca31146ef74b864cc00c76c
Moelf <jerryling315@gmail.com>
2020-10-15 20:06:09 -0400

    fix tests

--- test/rulesets/Base/fastmath_able.jl
+++ test/rulesets/Base/fastmath_able.jl
@@ -91,9 +91,15 @@ const FASTABLE_AST = quote
     end
 
     @testset "rounding" begin
-        for x in (-0.6, -0.2, -0.1, 0.1, 0.2, 0.6, 6.4)
-            test_scalar(floor, x, fdm = backward_fdm(5, 1))
-            test_scalar(ceil, x, fdm = forward_fdm(5, 1))
+        for x in (-0.6, -0.2, 0.1, 0.6)
+            # thanks to RoundNearest
+            if 0 > x % 1 > 0.5 || -1 < x % 1 <= 0.5
+                test_scalar(round, x; fdm=backward_fdm(5,1))
+            else
+                test_scalar(round, x; fdm=forward_fdm(5,1))
+            end
+            test_scalar(floor, x; fdm=backward_fdm(5, 1))
+            test_scalar(ceil, x; fdm=forward_fdm(5, 1))
         end
     end
 
