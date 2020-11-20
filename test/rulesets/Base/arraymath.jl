a0c50809c8689f11f36297e852c675541f18b5bf
Michael Abbott <me@escbook>
2020-11-20 09:56:56 +0100

    rm outer product case

--- test/rulesets/Base/arraymath.jl
+++ test/rulesets/Base/arraymath.jl
@@ -84,15 +84,6 @@
                     rrule_test(muladd, A*B.+z, (A, dA), (B, dB), (z, dz))
                 end
             end
-            if ndims(z) == 2
-                @testset "vector * adjoint" begin # outer product
-                    A, B = rand(T,3), rand(T,3)'
-                    dA, dB = rand(T,3), rand(T,3)'
-                    rrule_test(muladd, A*B.+z, (A, dA), (B, dB), (z, dz))
-                    dA, dB = rand(T,3), rand(T,1,3)
-                    rrule_test(muladd, A*B.+z, (A, dA), (B, dB), (z, dz))
-                end
-            end
         end
     end
     @testset "$f" for f in (/, \)
