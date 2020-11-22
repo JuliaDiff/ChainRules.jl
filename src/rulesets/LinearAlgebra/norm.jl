1b0895ca32d98fb94722d23901115ba31d5b49b2
Seth Axen <seth.axen@gmail.com>
2020-11-22 01:57:57 -0800

    Remove frules for norm

--- src/rulesets/LinearAlgebra/norm.jl
+++ src/rulesets/LinearAlgebra/norm.jl
@@ -2,26 +2,6 @@
 ##### `norm`
 #####
 
-function frule((_, Δx, Δp), ::typeof(norm), x, p::Real)
-    return if isempty(x)
-        z = float(norm(zero(eltype(x))))
-        z, zero(z * zero(Δp) + z * zero(eltype(Δx)))
-    elseif p == 2
-        _norm2_forward(x, Δx, Δp)
-    elseif p == 1
-        _norm1_forward(x, Δx, Δp)
-    elseif p == Inf
-        _normInf_forward(x, Δx, Δp; fnorm = LinearAlgebra.normInf)
-    elseif p == 0
-        z = typeof(float(norm(first(x))))(count(!iszero, x))
-        z, zero(z * zero(Δp) + z * zero(first(Δx)))
-    elseif p == -Inf
-        _normInf_forward(x, Δx, Δp; fnorm = LinearAlgebra.normMinusInf)
-    else
-        _normp_forward(x, p, Δx, Δp)
-    end
-end
-frule((Δself, Δx), ::typeof(norm), x) = frule((Δself, Δx, Zero()), norm, x, 2)
 function frule((_, Δx), ::typeof(norm), x::Number, p::Real=2)
     y = norm(x, p)
     ∂y = if iszero(Δx) || iszero(p)
@@ -31,15 +11,6 @@ function frule((_, Δx), ::typeof(norm), x::Number, p::Real=2)
         _realconjtimes(signx, Δx)
     end
     return y, ∂y
-end
-function frule(
-    (Δself, Δx, Δp),
-    ::typeof(norm),
-    x::Union{LinearAlgebra.TransposeAbsVec,LinearAlgebra.AdjointAbsVec},
-    p::Real,
-)
-    fdual = x isa Transpose ? transpose : adjoint
-    return frule((Δself, vec(fdual(Δx)), Δp), norm, parent(x), p)
 end
 
 function rrule(
@@ -116,8 +87,6 @@ end
 ##### `normp`
 #####
 
-frule((_, Δx, Δp), ::typeof(LinearAlgebra.normp), x, p) = _normp_forward(x, p, Δx, Δp)
-
 function rrule(
     ::typeof(LinearAlgebra.normp),
     x::Union{StridedArray,LinearAlgebra.AbstractTriangular,Diagonal},
@@ -131,37 +100,6 @@ function rrule(
     end
     normp_pullback(::Zero) = (NO_FIELDS, Zero(), Zero())
     return y, normp_pullback
-end
-
-function _normp_forward(x, p, Δx, Δp = Zero())
-    # TODO: accumulate `y` in parallel to `∂y`
-    y = LinearAlgebra.normp(x, p)
-    Δx isa AbstractZero && Δp isa AbstractZero && return (y, Zero())
-    x_Δx = zip(x, Δx isa AbstractZero ? Iterators.repeated(Δx) : Δx)
-    # non-differentiable wrt p at p ∈ {0, Inf}. use subgradient convention
-    ∂logp = ifelse(iszero(p) || isinf(p), zero(Δp) / one(p), Δp / p)
-    ((xi, Δxi), i) = iterate(x_Δx)::Tuple
-    ∂y = zero(real(Δxi)) / one(y) + zero(y)
-    y > 0 && isfinite(y) || return (y, zero(∂y))
-    if !isa(∂logp, AbstractZero)
-        ∂y -= y * log(y) * ∂logp
-    end
-    while true
-        a = norm(xi)
-        if !iszero(a)
-            signxi = xi isa Real ? sign(xi) : xi / a
-            ∂a = _realconjtimes(signxi, Δxi)
-            n = (a / y)^(p - 1)
-            ∂y += n * ∂a
-            if !isa(∂logp, AbstractZero) && !iszero(a)
-                ∂y += n * a * log(a) * ∂logp
-            end
-        end
-        state = iterate(x_Δx, i)
-        state === nothing && break
-        ((xi, Δxi), i) = state
-    end
-    return y, ∂y
 end
 
 function _normp_back_x(x, p, y, Δy)
@@ -190,14 +128,6 @@ end
 ##### `normMinusInf`/`normInf`
 #####
 
-function frule((_, Δx), ::typeof(LinearAlgebra.normMinusInf), x)
-    return _normInf_forward(x, Δx; fnorm = LinearAlgebra.normMinusInf)
-end
-
-function frule((_, Δx), ::typeof(LinearAlgebra.normInf), x)
-    return _normInf_forward(x, Δx; fnorm = LinearAlgebra.normInf)
-end
-
 function rrule(
     ::typeof(LinearAlgebra.normMinusInf),
     x::Union{StridedArray,LinearAlgebra.AbstractTriangular,Diagonal},
@@ -218,29 +148,6 @@ function rrule(
     return y, normInf_pullback
 end
 
-function _normInf_forward(x, Δx, Δp = Zero(); fnorm = LinearAlgebra.normInf)
-    Δx isa AbstractZero && return (fnorm(x), Zero())
-    x_Δx = zip(x, Δx)
-    cmp = fnorm === LinearAlgebra.normInf ? (>) : (<)
-    ((xi, Δxi), i) = iterate(x_Δx)::Tuple
-    y = norm(xi)
-    ∂y = _realconjtimes(sign(xi), Δxi)
-    while true
-        state = iterate(x_Δx, i)
-        state === nothing && break
-        ((xi, Δxi), i) = state
-        a = norm(xi)
-        # if multiple `xi`s have the exact same norm, then the corresponding `Δxi`s must
-        # be identical if upstream rules behaved correctly, so any `Δxi` will do.
-        (y, ∂y) = ifelse(
-            isnan(y) | cmp(y, a),
-            (y, ∂y),
-            (a, _realconjtimes(sign(xi), Δxi)),
-        )
-    end
-    return float(y), float(∂y)
-end
-
 function _normInf_back(x, y, Δy)
     Δu = real(Δy)
     T = typeof(zero(float(eltype(x))) * zero(Δu))
@@ -258,8 +165,6 @@ end
 ##### `norm1`
 #####
 
-frule((_, Δx), ::typeof(LinearAlgebra.norm1), x) = _norm1_forward(x, Δx)
-
 function rrule(
     ::typeof(LinearAlgebra.norm1),
     x::Union{StridedArray,LinearAlgebra.AbstractTriangular,Diagonal},
@@ -270,45 +175,11 @@ function rrule(
     return y, norm1_pullback
 end
 
-function _norm1_forward(x, Δx, Δp = Zero())
-    Δx isa AbstractZero && return (LinearAlgebra.norm1(x), Zero())
-    x_Δx = zip(x, Δx)
-    ((xi, Δxi), i) = iterate(x_Δx)::Tuple
-    a = float(norm(xi))
-    T = typeof(a)
-    y::promote_type(Float64, T) = a
-    signxi = xi isa Real ? sign(xi) : xi / ifelse(iszero(a), one(a), a)
-    ∂a = _realconjtimes(signxi, Δxi)
-    T∂ = typeof(zero(∂a))
-    ∂y::promote_type(Float64, T∂) = ∂a
-    if !isa(Δp, AbstractZero) && !iszero(a)
-        ∂y += a * log(a) * Δp
-    end
-    while true
-        state = iterate(x_Δx, i)
-        state === nothing && break
-        ((xi, Δxi), i) = state
-        a = norm(xi)
-        y += a
-        signxi = xi isa Real ? sign(xi) : xi / ifelse(iszero(a), one(a), a)
-        ∂y += _realconjtimes(signxi, Δxi)
-        if !isa(Δp, AbstractZero) && !iszero(a)
-            ∂y += a * log(a) * Δp
-        end
-    end
-    if !isa(Δp, AbstractZero) && !iszero(y) && isfinite(y)
-        ∂y -= y * log(y) * Δp
-    end
-    return convert(T, y), convert(T∂, ∂y)
-end
-
 _norm1_back(x, y, Δy) = sign.(x) .* real(Δy)
 
 #####
 ##### `norm2`
 #####
-
-frule((_, Δx), ::typeof(LinearAlgebra.norm2), x) = _norm2_forward(x, Δx)
 
 function rrule(
     ::typeof(LinearAlgebra.norm2),
@@ -320,46 +191,11 @@ function rrule(
     return y, norm2_pullback
 end
 
-function _norm2_forward(x, Δx, Δp = Zero())
-    y = LinearAlgebra.norm2(x)
-    # since dot product is efficient for pushforward, we don't accumulate in parallel
-    ∂y = Δx isa AbstractZero ? Δx : real(dot(x, Δx)) * pinv(y)
-    if !isa(Δp, AbstractZero) && !iszero(y)
-        s = sum(x) do xi
-            a = abs2(xi)
-            return ifelse(iszero(a), zero(float(a)), a * log(a))
-        end
-        ∂y += (s / y / 2 - y * log(y)) * Δp / 2
-    end
-    return y, ∂y
-end
-
 _norm2_back(x, y, Δy) = x .* (real(Δy) * pinv(y))
 
 #####
-##### `normalize`/`normalize!`
+##### `normalize`
 #####
-
-function frule((_, Δx, Δp), ::typeof(normalize!), x::AbstractVector, p::Real)
-    (nrm, ∂nrm) = frule((Zero(), Δx, Δp), norm, x, p)
-    _normalize_forward!(x, nrm, Δx, ∂nrm)
-    return (x, Δx)
-end
-function frule((Δself, Δx), ::typeof(normalize!), x::AbstractVector)
-    return frule((Δself, Δx, Zero()), normalize!, x, 2)
-end
-
-function frule((_, Δx, Δp), ::typeof(normalize), x::AbstractVector, p::Real)
-    (nrm, ∂nrm) = frule((Zero(), Δx, Δp), norm, x, p)
-    T = typeof(first(x) / nrm)
-    y = copyto!(similar(x, T), x)
-    ∂y = copyto!(similar(Δx, typeof(one(T) * ∂nrm + first(Δx))), Δx)
-    _normalize_forward!(y, nrm, ∂y, ∂nrm)
-    return y, ∂y
-end
-function frule((Δself, Δx), ::typeof(normalize), x::AbstractVector)
-    return frule((Δself, Δx, Zero()), normalize, x, 2)
-end
 
 function rrule(::typeof(normalize), x::AbstractVector, p::Real)
     nrm, inner_pullback = rrule(norm, x, p)
@@ -385,13 +221,4 @@ function rrule(::typeof(normalize), x::AbstractVector)
         return (NO_FIELDS, ∂x)
     end
     return y, normalize_pullback
-end
-
-function _normalize_forward!(x, nrm, Δx, Δnrm)
-    LinearAlgebra.__normalize!(x, nrm)
-    @inbounds for i in eachindex(x, Δx)
-        Δx[i] -= x[i] * Δnrm
-    end
-    LinearAlgebra.__normalize!(Δx, nrm)
-    return x, Δx
 end
--- test/rulesets/LinearAlgebra/norm.jl
+++ test/rulesets/LinearAlgebra/norm.jl
@@ -29,50 +29,15 @@
             x[3] = rand(T)
         end
 
-        @testset "frule" begin
-            xiter = TestIterator(x, Base.HasLength(), Base.HasEltype())
-            ẋ = rand_tangent(x)
-            ẋiter = rand_tangent(xiter)
-            ṗ = rand_tangent(p)
-            pṗ = isempty(p) ? () : ((p, ṗ),)
+        y = fnorm(x, p...)
+        x̄ = rand_tangent(x)
+        ȳ = rand_tangent(y)
+        pp̄ = isempty(p) ? () : ((p, rand_tangent(p)),)
 
-            if isempty(x) # finite differences can't handle empty x
-                @test frule((Zero(), ẋ, ṗ), fnorm, x, p...) == (zero(T), zero(T))
-                @testset "iterator" begin
-                    @test frule(
-                        (Zero(), ẋiter, ṗ),
-                        fnorm,
-                        xiter,
-                        p...,
-                    ) == (zero(T), zero(T))
-                end
-                @test frule(
-                    (Zero(), Zero(), Zero()),
-                    fnorm,
-                    x,
-                    p...,
-                ) == (zero(T), Zero())
-                continue
-            end
-
-            frule_test(norm, (x, ẋ), pṗ...)
-            @testset "iterator" begin
-                frule_test(norm, (xiter, ẋiter), pṗ...)
-            end
-            @test frule((Zero(), ẋ, ṗ), norm, zero(x), p...)[2] ≈ 0
-            @test frule((Zero(), Zero(), Zero()), norm, x, p...)[2] isa Zero
-        end
-        @testset "rrule" begin
-            y = fnorm(x, p...)
-            x̄ = rand_tangent(x)
-            ȳ = rand_tangent(y)
-            pp̄ = isempty(p) ? () : ((p, rand_tangent(p)),)
-
-            # fd has stability issues for the norm functions, so we use high order 10
-            rrule_test(norm, ȳ, (x, x̄), pp̄...; fdm = central_fdm(10, 1))
-            @test extern(rrule(norm, zero(x), p...)[2](ȳ)[2]) ≈ zero(x)
-            @test rrule(norm, x, p...)[2](Zero())[2] isa Zero
-        end
+        # fd has stability issues for the norm functions, so we use high order 10
+        rrule_test(norm, ȳ, (x, x̄), pp̄...; fdm = central_fdm(10, 1))
+        @test extern(rrule(norm, zero(x), p...)[2](ȳ)[2]) ≈ zero(x)
+        @test rrule(norm, x, p...)[2](Zero())[2] isa Zero
     end
     @testset "norm(x::$T[, p])" for T in (Float64, ComplexF64)
         @testset "norm(x::$T)" for T in (Float64, ComplexF64)
@@ -82,9 +47,7 @@
         @testset "p = $p" for p in (-1.0, 1.5, 2.0)
             x = randn(T)
             y = norm(x, p)
-            ẋ, ṗ = rand_tangent.((x, p))
             x̄, p̄, ȳ = rand_tangent.((x, p, y))
-            frule_test(norm, (x, ẋ), (p, ṗ))
             rrule_test(norm, ȳ, (x, x̄), (p, p̄))
             _, back = rrule(norm, x, p)
             @test back(Zero()) == (NO_FIELDS, Zero(), Zero())
@@ -93,11 +56,7 @@
             p = 0.0
             x = randn(T)
             y = norm(x, p)
-            ẋ, ṗ = rand_tangent.((x, p))
             x̄, p̄, ȳ = rand_tangent.((x, p, y))
-            y_fwd, ẏ_fwd = frule((Zero(), ẋ, ṗ), norm, x, p)
-            @test y_fwd == y
-            @test iszero(ẏ_fwd)
             y_rev, back = rrule(norm, x, p)
             @test y_rev == y
             @test back(ȳ) == (NO_FIELDS, zero(x), Zero())
@@ -105,34 +64,13 @@
         end
     end
 end
-@testset "$f with p=$p" for f in (normalize, normalize!), T in (Float64, ComplexF64),
+@testset "normalize with T=$T, p=$p" for T in (Float64, ComplexF64),
     p in ((), 1.0, 2.0, -Inf, Inf, 1.5) # skip p=0, since FD is unstable
-
     n = 3
-    @testset "frule" begin
-        x = randn(T, n)
-        ẋ = rand_tangent(x)
-        ṗ = rand_tangent(p)
-        pṗ = isempty(p) ? () : ((p, ṗ),)
-        y = f(copy(x), p...)
-        # `frule_test` doesn't handle mutating functions yet
-        xcopy = copy(x)
-        ẋcopy = copy(ẋ)
-        (y_fwd, ẏ_fwd) = frule((Zero(), ẋcopy, ṗ), f, xcopy, p...)
-        if f === normalize!
-            @test y_fwd === xcopy
-            @test ẏ_fwd === ẋcopy
-        end
-        @test y_fwd ≈ y
-        ẏ_fd = jvp(_fdm, (x, p...) -> f(copy(x), p...), (x, ẋ), pṗ...)
-        @test ẏ_fwd ≈ ẏ_fd
-    end
-    f === normalize && @testset "rrule" begin
-        x = randn(T, n)
-        y = f(copy(x), p...)
-        x̄ = rand_tangent(x)
-        ȳ = rand_tangent(y)
-        pp̄ = isempty(p) ? () : ((p, rand_tangent(p)),)
-        rrule_test(f, ȳ, (x, x̄), pp̄...)
-    end
+    x = randn(T, n)
+    y = normalize(copy(x), p...)
+    x̄ = rand_tangent(x)
+    ȳ = rand_tangent(y)
+    pp̄ = isempty(p) ? () : ((p, rand_tangent(p)),)
+    rrule_test(normalize, ȳ, (x, x̄), pp̄...)
 end
