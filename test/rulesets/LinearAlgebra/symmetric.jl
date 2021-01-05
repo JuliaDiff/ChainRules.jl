e257b11e65a3dd8a8ff0a389a0e2c707d2cce8ab
Seth Axen <seth.axen@gmail.com>
2021-01-05 01:45:06 -0800

    Remove all eigendecomposition rules
    Moved to https://github.com/JuliaDiff/ChainRules.jl/pull/323

--- test/rulesets/LinearAlgebra/symmetric.jl
+++ test/rulesets/LinearAlgebra/symmetric.jl
@@ -1,14 +1,3 @@
-# get diagonal matrix D such that vectors * D produces equivalent eigenvectors with the
-# first row entirely real. This stabilizes eigenvectors for finite differences
-_eigvecs_standardize_mat(vectors) = Diagonal(conj.(sign.(@view(vectors[1, :]))))
-
-# eigen but with deterministic sign convention for the eigenvectors
-function _eigen_stable(A)
-    F = eigen(A)
-    rmul!(F.vectors, _eigvecs_standardize_mat(F.vectors))
-    return F
-end
-
 @testset "Symmetric/Hermitian rules" begin
     @testset "$(SymHerm)(::AbstractMatrix{$T}, :$(uplo))" for
         SymHerm in (Symmetric, Hermitian),
@@ -51,84 +40,6 @@ end
         frule_test(f, (x, Δx))
         frule_test(f, (x, SymHerm(Δx, uplo)))
         rrule_test(f, ΔΩ, (x, ∂x))
-    end
-
-    @testset "eigen(::$T{<:Real})" for T in (Symmetric, Hermitian)
-        n = 5
-        @testset "frule" begin
-            @testset for uplo in (:L, :U)
-                A, ΔA = T(randn(n, n), uplo), T(randn(n, n), uplo)
-                F = eigen(A)
-                F_ad, ∂F_ad = frule((Zero(), ΔA), eigen, A)
-                @test F_ad == F
-                ∂F_ad = unthunk(∂F_ad)
-                @test ∂F_ad isa Composite{typeof(F)}
-                @test ∂F_ad.values ≈ jvp(_fdm, A -> eigen(T(A, uplo)).values, (A.data, ΔA.data))
-                # TODO: adopt a deterministic sign convention to stabilize the FD estimate
-                P = _eigvecs_standardize_mat(F.vectors)
-                @test ∂F_ad.vectors ≈ jvp(_fdm, A -> _eigen_stable(T(A, uplo)).vectors, (A.data, ΔA.data)) / P
-            end
-        end
-
-        @testset "rrule" begin
-            @testset for uplo in (:L, :U)
-                A, ΔU, Δλ = randn(n, n), randn(n, n), randn(n)
-                symA = T(A, uplo)
-                F = eigen(symA)
-                ΔF = Composite{typeof(F)}(values = Δλ, vectors = ΔU)
-                F_ad, back = rrule(eigen, symA)
-                @test F_ad == F
-                ∂self, ∂symA = back(ΔF)
-                @test ∂self === NO_FIELDS
-                ∂symA = unthunk(∂symA)
-                @test ∂symA isa typeof(symA)
-                @test ∂symA.uplo == symA.uplo
-                # pull the cotangent back to A to test against finite differences
-                ∂A = unthunk(rrule(T, A, uplo)[2](∂symA)[2])
-                # adopt a deterministic sign convention to stabilize FD
-                P = _eigvecs_standardize_mat(F.vectors)
-                ΔF_stable = Composite{typeof(F)}(values = Δλ, vectors = ΔU / P')
-                f = function (A)
-                    F = _eigen_stable(T(A, uplo))
-                    return (values = F.values, vectors = F.vectors)
-                end
-                ∂A_fd = j′vp(_fdm, f, ΔF_stable, A)[1]
-                @test ∂A ≈ ∂A_fd
-            end
-        end
-    end
-
-    @testset "eigvals(::$T{<:Real})" for T in (Symmetric, Hermitian)
-        n = 5
-        @testset "frule" begin
-            @testset for uplo in (:L, :U)
-                A, ΔA = T(randn(n, n), uplo), T(randn(n, n), uplo)
-                λ = eigvals(A)
-                λ_ad, ∂λ_ad = frule((Zero(), ΔA), eigvals, A)
-                @test λ_ad ≈ λ # inexact because frule uses eigen not eigvals
-                ∂λ_ad = unthunk(∂λ_ad)
-                @test ∂λ_ad isa typeof(λ)
-                @test ∂λ_ad ≈ jvp(_fdm, A -> eigvals(T(A, uplo)), (A.data, ΔA.data))
-            end
-        end
-
-        @testset "rrule" begin
-            @testset for uplo in (:L, :U)
-                A, Δλ = randn(n, n), randn(n)
-                symA = T(A, uplo)
-                λ = eigvals(symA)
-                λ_ad, back = rrule(eigvals, symA)
-                @test λ_ad ≈ λ # inexact because rrule uses eigen not eigvals
-                ∂self, ∂symA = back(Δλ)
-                @test ∂self === NO_FIELDS
-                ∂symA = unthunk(∂symA)
-                @test ∂symA isa typeof(symA)
-                @test ∂symA.uplo == symA.uplo
-                # pull the cotangent back to A to test against finite differences
-                ∂A = unthunk(rrule(T, A, uplo)[2](∂symA)[2])
-                @test ∂A ≈ j′vp(_fdm, A -> eigvals(T(A, uplo)), Δλ, A)[1]
-            end
-        end
     end
 
     # TODO: add tests for
