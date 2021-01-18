b48204c60552b48be11da3faa9ed0f92f64bda53
Seth Axen <seth.axen@gmail.com>
2021-01-18 03:14:33 -0800

    Test rrule

--- test/rulesets/LinearAlgebra/matfun.jl
+++ test/rulesets/LinearAlgebra/matfun.jl
@@ -13,4 +13,24 @@
             frule_test(LinearAlgebra.exp!, (A, ΔA))
         end
     end
+
+    @testset "exp(A::Matrix) rrule" begin
+        n = 10
+        @testset "A::Matrix{$T}, opnorm(A,1)=$nrm" for T in (Float64, ComplexF64), nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
+            A, ΔA = randn(ComplexF64, n, n), randn(ComplexF64, n, n)
+            ΔY = randn(ComplexF64, n, n)
+            # choose normalization to hit specific branch
+            A *= nrm / opnorm(A, 1)
+            # rrule is not inferrable, but pullback should be
+            rrule_test(exp, ΔY, (A, ΔA); check_inferred = false)
+            Y, back = rrule(exp, A)
+            @inferred back(ΔY)
+        end
+        @testset "hermitian A" begin
+            A, ΔA = Matrix(Hermitian(randn(ComplexF64, n, n))), randn(ComplexF64, n, n)
+            ΔY = randn(ComplexF64, n, n)
+            rrule_test(exp, Matrix(Hermitian(ΔY)), (A, ΔA); check_inferred = false)
+            rrule_test(exp, ΔY, (A, ΔA); check_inferred = false)
+        end
+    end
 end
