dc1b1ab47cdbd460e4137efc89614ac2c1adc3ec
Seth Axen <seth.axen@gmail.com>
2021-01-19 11:49:12 -0800

    Apply suggestions from code review
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>

--- src/rulesets/LinearAlgebra/matfun.jl
+++ src/rulesets/LinearAlgebra/matfun.jl
@@ -1,4 +1,5 @@
 # matrix functions of dense matrices
+# https://en.wikipedia.org/wiki/Matrix_function
 
 # NOTE: for matrix functions whose power series representation has real coefficients,
 # the pullback and pushforward are related by an adjoint.
@@ -86,6 +87,7 @@ end
 ## Destructive matrix exponential using algorithm from Higham, 2008,
 ## "Functions of Matrices: Theory and Computation", SIAM
 ## Adapted from LinearAlgebra.exp! with return of intermediates
+## https://github.com/JuliaLang/julia/blob/f613b551009a7a9dbe46235929099f2ecd28bed1/stdlib/LinearAlgebra/src/dense.jl#L583-L666
 function _matfun!(::typeof(exp), A::StridedMatrix{T}) where {T<:BlasFloat}
     n = LinearAlgebra.checksquare(A)
     ilo, ihi, scale = LAPACK.gebal!('B', A)  # modifies A
--- test/rulesets/LinearAlgebra/matfun.jl
+++ test/rulesets/LinearAlgebra/matfun.jl
@@ -2,7 +2,7 @@
     @testset "LinearAlgebra.exp!(A::Matrix) frule" begin
         n = 10
         @testset "A::Matrix{$T}, opnorm(A,1)=$nrm" for T in (Float64, ComplexF64),
-nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
+            nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
 
             A, ΔA = randn(ComplexF64, n, n), randn(ComplexF64, n, n)
             # choose normalization to hit specific branch
@@ -15,7 +15,8 @@ nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
             frule_test(LinearAlgebra.exp!, (A, ΔA))
         end
         @testset "hermitian A" begin
-            A, ΔA = Matrix(Hermitian(randn(ComplexF64, n, n))), randn(ComplexF64, n, n)
+            A = Matrix(Hermitian(randn(ComplexF64, n, n)))
+            ΔA = randn(ComplexF64, n, n)
             frule_test(LinearAlgebra.exp!, (A, Matrix(Hermitian(ΔA))))
             frule_test(LinearAlgebra.exp!, (A, ΔA))
         end
@@ -24,7 +25,7 @@ nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
     @testset "exp(A::Matrix) rrule" begin
         n = 10
         @testset "A::Matrix{$T}, opnorm(A,1)=$nrm" for T in (Float64, ComplexF64),
-nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
+            nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
 
             A, ΔA = randn(ComplexF64, n, n), randn(ComplexF64, n, n)
             ΔY = randn(ComplexF64, n, n)
