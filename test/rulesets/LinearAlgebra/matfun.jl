9a63d13704213dff23d1857006584b0c0ee5d93d
Seth Axen <seth.axen@gmail.com>
2021-01-19 21:18:56 -0800

    Move comment up

--- test/rulesets/LinearAlgebra/matfun.jl
+++ test/rulesets/LinearAlgebra/matfun.jl
@@ -2,10 +2,10 @@
     @testset "LinearAlgebra.exp!(A::Matrix) frule" begin
         n = 10
         @testset "A::Matrix{$T}, opnorm(A,1)=$nrm" for T in (Float64, ComplexF64),
+            # choose normalization to hit specific branch
             nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
 
             A, ΔA = randn(ComplexF64, n, n), randn(ComplexF64, n, n)
-            # choose normalization to hit specific branch
             A *= nrm / opnorm(A, 1)
             frule_test(LinearAlgebra.exp!, (A, ΔA))
         end
@@ -25,11 +25,11 @@
     @testset "exp(A::Matrix) rrule" begin
         n = 10
         @testset "A::Matrix{$T}, opnorm(A,1)=$nrm" for T in (Float64, ComplexF64),
+            # choose normalization to hit specific branch
             nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
 
             A, ΔA = randn(ComplexF64, n, n), randn(ComplexF64, n, n)
             ΔY = randn(ComplexF64, n, n)
-            # choose normalization to hit specific branch
             A *= nrm / opnorm(A, 1)
             # rrule is not inferrable, but pullback should be
             rrule_test(exp, ΔY, (A, ΔA); check_inferred=false)
