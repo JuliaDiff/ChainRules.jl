98ccda70d6e61ab175e4dae43682af82f7343907
Seth Axen <seth.axen@gmail.com>
2021-01-22 08:19:38 -0800

    Correctly test real matrix exponential (#353)
    * Actually test real case
    
    * Increase tolerance for nrm1 = 0.1

--- test/rulesets/LinearAlgebra/matfun.jl
+++ test/rulesets/LinearAlgebra/matfun.jl
@@ -5,18 +5,20 @@
         @testset "A::Matrix{$T}, opnorm(A,1)=$nrm" for T in (Float64, ComplexF64),
             nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
 
-            A, ΔA = randn(ComplexF64, n, n), randn(ComplexF64, n, n)
+            A = randn(T, n, n)
+            ΔA = randn(T, n, n)
             A *= nrm / opnorm(A, 1)
-            frule_test(LinearAlgebra.exp!, (A, ΔA))
+            tols = nrm == 0.1 ? (atol=1e-8, rtol=1e-8) : NamedTuple()
+            frule_test(LinearAlgebra.exp!, (A, ΔA); tols...)
         end
         @testset "imbalanced A" begin
             A = Float64[0 10 0 0; -1 0 0 0; 0 0 0 0; -2 0 0 0]
             ΔA = rand_tangent(A)
             frule_test(LinearAlgebra.exp!, (A, ΔA))
         end
-        @testset "hermitian A" begin
-            A = Matrix(Hermitian(randn(ComplexF64, n, n)))
-            ΔA = randn(ComplexF64, n, n)
+        @testset "hermitian A, T=$T" for T in (Float64, ComplexF64)
+            A = Matrix(Hermitian(randn(T, n, n)))
+            ΔA = randn(T, n, n)
             frule_test(LinearAlgebra.exp!, (A, Matrix(Hermitian(ΔA))))
             frule_test(LinearAlgebra.exp!, (A, ΔA))
         end
@@ -28,11 +30,13 @@
         @testset "A::Matrix{$T}, opnorm(A,1)=$nrm" for T in (Float64, ComplexF64),
             nrm in (0.01, 0.1, 0.5, 1.5, 3.0, 6.0, 12.0)
 
-            A, ΔA = randn(ComplexF64, n, n), randn(ComplexF64, n, n)
-            ΔY = randn(ComplexF64, n, n)
+            A = randn(T, n, n)
+            ΔA = randn(T, n, n)
+            ΔY = randn(T, n, n)
             A *= nrm / opnorm(A, 1)
             # rrule is not inferrable, but pullback should be
-            rrule_test(exp, ΔY, (A, ΔA); check_inferred=false)
+            tols = nrm == 0.1 ? (atol=1e-8, rtol=1e-8) : NamedTuple()
+            rrule_test(exp, ΔY, (A, ΔA); check_inferred=false, tols...)
             Y, back = rrule(exp, A)
             @inferred back(ΔY)
         end
@@ -42,9 +46,10 @@
             ΔY = rand_tangent(exp(A))
             rrule_test(exp, ΔY, (A, ΔA); check_inferred=false)
         end
-        @testset "hermitian A" begin
-            A, ΔA = Matrix(Hermitian(randn(ComplexF64, n, n))), randn(ComplexF64, n, n)
-            ΔY = randn(ComplexF64, n, n)
+        @testset "hermitian A, T=$T" for T in (Float64, ComplexF64)
+            A = Matrix(Hermitian(randn(T, n, n)))
+            ΔA = randn(T, n, n)
+            ΔY = randn(T, n, n)
             rrule_test(exp, Matrix(Hermitian(ΔY)), (A, ΔA); check_inferred=false)
             rrule_test(exp, ΔY, (A, ΔA); check_inferred=false)
         end
