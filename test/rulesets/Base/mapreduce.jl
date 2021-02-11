e01c7128b7af3c53ebe012eaf298822a377b5132
Miha Zgubic <miha.zgubic@invenialabs.co.uk>
2021-02-11 20:05:47 +0000

    resolve mapreduce 2

--- test/rulesets/Base/mapreduce.jl
+++ test/rulesets/Base/mapreduce.jl
@@ -2,7 +2,6 @@
     @testset "sum" begin
         sizes = (3, 4, 7)
         @testset "dims = $dims" for dims in (:, 1)
-            fkwargs = (dims=dims,)
             @testset "Array{$N, $T}" for N in eachindex(sizes), T in (Float64, ComplexF64)
                 x = randn(T, sizes[1:N]...)
                 test_frule(sum, x; fkwargs=(;dims=dims))
@@ -14,7 +13,6 @@
     @testset "sum abs2" begin
         sizes = (3, 4, 7)
         @testset "dims = $dims" for dims in (:, 1)
-            fkwargs = (dims=dims,)
             @testset "Array{$N, $T}" for N in eachindex(sizes), T in (Float64, ComplexF64)
                 x = randn(T, sizes[1:N]...)
                 test_frule(sum, abs2, x; fkwargs=(;dims=dims))
