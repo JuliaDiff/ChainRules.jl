3cdd2d6c0a9f42864e9c7e7832a9c5bdac8a99fe
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-03-16 14:07:34 +0530

    style cleanups

--- src/rulesets/SparseArrays/sparsematrix.jl
+++ src/rulesets/SparseArrays/sparsematrix.jl
@@ -2,14 +2,14 @@ using SparseArrays
 
 function rrule(::Type{<:SparseMatrixCSC{T,N}}, arr) where {T,N}
     function SparseMatrix_pullback(Δ)
-      return NO_FIELDS, collect(Δ)
+        return NO_FIELDS, collect(Δ)
     end
     return SparseMatrixCSC{T,N}(arr), SparseMatrix_pullback
 end
 
 function rrule(::typeof(Matrix), x::SparseMatrixCSC)
   function Matrix_pullback(Δ)
-    NO_FIELDS, Δ
+      NO_FIELDS, Δ
   end
-  Matrix(x), Matrix_pullback
+  return Matrix(x), Matrix_pullback
 end
--- test/rulesets/SparseArrays/sparsematrix.jl
+++ test/rulesets/SparseArrays/sparsematrix.jl
@@ -1,7 +1,7 @@
 using SparseArrays
 
 @testset "Sparse" begin
-  r = sparse(rand(3,3))
-  x, x̄ = rand(3,3), rand(3,3)
-  rrule_test(sparse, r, (x, x̄))
+    r = sparse(rand(3,3))
+    x, x̄ = rand(3,3), rand(3,3)
+    rrule_test(sparse, r, (x, x̄))
 end
