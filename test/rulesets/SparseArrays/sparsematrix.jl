dd392a8a49c12d9847cd4eb2702c83460a255dcd
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-03-16 14:16:50 +0530

    use test_rrule instead

--- test/rulesets/SparseArrays/sparsematrix.jl
+++ test/rulesets/SparseArrays/sparsematrix.jl
@@ -3,5 +3,6 @@ using SparseArrays
 @testset "Sparse" begin
     r = sparse(rand(3,3))
     x, x̄ = rand(3,3), rand(3,3)
-    rrule_test(sparse, r, (x, x̄))
+    test_rrule(sparse, r)
+    test_rrule(Matrix, r)
 end
