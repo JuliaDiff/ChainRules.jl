82bec3dfe827446d73ac7e066d486e2191086a6c
Dhairya Gandhi <dhairya@juliacopmuting.com>
2021-03-16 14:53:55 +0530

    test for SparseMatricCSC instead

--- test/rulesets/SparseArrays/sparsematrix.jl
+++ test/rulesets/SparseArrays/sparsematrix.jl
@@ -3,6 +3,6 @@ using SparseArrays
 @testset "Sparse" begin
     r = sparse(rand(3,3))
     x, x̄ = rand(3,3), rand(3,3)
-    test_rrule(sparse, r)
+    test_rrule(SparseMatrixCSC, r)
     test_rrule(Matrix, r)
 end
