f8eeb7a32a2ea69ddb816a2fe637bf333b6f4cb2
Dhairya Gandhi <dhairya@juliacomputing.com>
2021-04-18 01:11:22 +0530

    fix tests

--- test/rulesets/Base/base.jl
+++ test/rulesets/Base/base.jl
@@ -170,7 +170,7 @@
     end
 
     @testset "Logging" begin
-        @test frule((NO_FIELDS, DoesNotExist(), DoesNotExist()), Base.depwarn, "message", x -> x) !== nothing
-        @test rrule(Base.depwarn, "message", x -> x) !== nothing
+        @test frule((NO_FIELDS, DoesNotExist(), DoesNotExist()), Base.depwarn, "message", :f) !== nothing
+        @test rrule(Base.depwarn, "message", :f) !== nothing
     end
 end
