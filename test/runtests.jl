1cbf58e12eb860c5490caf4fb5b0a6b3187a5b1f
Simeon Schaub <simeondavidschaub99@gmail.com>
2021-06-02 17:17:17 +0200

    RFC: use multiple threads for tests
    Not sure this is the right way to do this and how much this will end up
    screwing with the test output, but let's just see for now whether this
    actually makes CI finish faster.

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,47 +15,61 @@ using Statistics
 using Test
 
 Random.seed!(1) # Set seed that all testsets should reset to.
+BLAS.set_num_threads(1)
 
 function include_test(path)
     println("Testing $path:")  # print so TravisCI doesn't timeout due to no output
     @time include(path)  # show basic timing, (this will print a newline at end)
 end
 
+if VERSION >= v"1.3"
+    using Base.Threads: @spawn, @sync
+else
+    macro spawn(ex) esc(ex) end
+    macro sync(ex) esc(ex) end
+end
+
 println("Testing ChainRules.jl")
 @testset "ChainRules" begin
     @testset "rulesets" begin
-        @testset "Base" begin
-            include_test("rulesets/Base/base.jl")
-            include_test("rulesets/Base/fastmath_able.jl")
-            include_test("rulesets/Base/evalpoly.jl")
-            include_test("rulesets/Base/array.jl")
-            include_test("rulesets/Base/arraymath.jl")
-            include_test("rulesets/Base/indexing.jl")
-            include_test("rulesets/Base/mapreduce.jl")
-            include_test("rulesets/Base/sort.jl")
-        end
-        println()
+        @sync begin
+            @testset "Base" begin
+                @sync begin
+                    @spawn include_test("rulesets/Base/base.jl")
+                    @spawn include_test("rulesets/Base/fastmath_able.jl")
+                    @spawn include_test("rulesets/Base/evalpoly.jl")
+                    @spawn include_test("rulesets/Base/array.jl")
+                    @spawn include_test("rulesets/Base/arraymath.jl")
+                    @spawn include_test("rulesets/Base/indexing.jl")
+                    @spawn include_test("rulesets/Base/mapreduce.jl")
+                    @spawn include_test("rulesets/Base/sort.jl")
+                end
+            end
+            println()
 
-        @testset "Statistics" begin
-            include_test("rulesets/Statistics/statistics.jl")
-        end
-        println()
+            Threads.@spawn @testset "Statistics" begin
+                include_test("rulesets/Statistics/statistics.jl")
+            end
+            println()
 
-        @testset "LinearAlgebra" begin
-            include_test("rulesets/LinearAlgebra/dense.jl")
-            include_test("rulesets/LinearAlgebra/norm.jl")
-            include_test("rulesets/LinearAlgebra/matfun.jl")
-            include_test("rulesets/LinearAlgebra/structured.jl")
-            include_test("rulesets/LinearAlgebra/symmetric.jl")
-            include_test("rulesets/LinearAlgebra/factorization.jl")
-            include_test("rulesets/LinearAlgebra/blas.jl")
-            include_test("rulesets/LinearAlgebra/lapack.jl")
-        end
-        println()
+            @testset "LinearAlgebra" begin
+                @sync begin
+                    @spawn include_test("rulesets/LinearAlgebra/dense.jl")
+                    @spawn include_test("rulesets/LinearAlgebra/norm.jl")
+                    @spawn include_test("rulesets/LinearAlgebra/matfun.jl")
+                    @spawn include_test("rulesets/LinearAlgebra/structured.jl")
+                    @spawn include_test("rulesets/LinearAlgebra/symmetric.jl")
+                    @spawn include_test("rulesets/LinearAlgebra/factorization.jl")
+                    @spawn include_test("rulesets/LinearAlgebra/blas.jl")
+                    @spawn include_test("rulesets/LinearAlgebra/lapack.jl")
+                end
+            end
+            println()
 
-        @testset "Random" begin
-            include_test("rulesets/Random/random.jl")
+            @spawn @testset "Random" begin
+                include_test("rulesets/Random/random.jl")
+            end
+            println()
         end
-        println()
     end
 end
