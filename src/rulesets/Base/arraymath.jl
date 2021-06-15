e2b45e5ecf41dd7ab38bb2fd539d3ff8b749a17f
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-06-15 15:29:21 -0400

    version bound defn

--- src/rulesets/Base/arraymath.jl
+++ src/rulesets/Base/arraymath.jl
@@ -132,6 +132,8 @@ end
 ##### fused 3-argument *
 #####
 
+if VERSION > v"1.7.0-DEV.1284"
+
 using LinearAlgebra: mat_mat_scalar, mat_vec_scalar, rmul!, StridedMaybeAdjOrTransMat
 
 function rrule(
@@ -199,6 +201,8 @@ function rrule(
     end
     return y, mat_vec_scalar_back
 end
+
+end # VERSION
 
 #####
 ##### `muladd`
