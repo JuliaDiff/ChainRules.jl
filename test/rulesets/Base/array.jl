8060bd5b8416958bbbe035b12f9b77ddb1ccdac2
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-06-23 13:51:51 -0400

    rm inferred checks on 1.0

--- test/rulesets/Base/array.jl
+++ test/rulesets/Base/array.jl
@@ -5,9 +5,9 @@
 end
 
 @testset "hcat" begin
-    test_rrule(hcat, randn(3, 2), randn(3), randn(3, 3))
-    test_rrule(hcat, rand(), rand(1,2), rand(1,2,1))
-    test_rrule(hcat, rand(3,1,1,2), rand(3,3,1,2))
+    test_rrule(hcat, randn(3, 2), randn(3), randn(3, 3); check_inferred=VERSION>v"1.1")
+    test_rrule(hcat, rand(), rand(1,2), rand(1,2,1); check_inferred=VERSION>v"1.1")
+    test_rrule(hcat, rand(3,1,1,2), rand(3,3,1,2); check_inferred=VERSION>v"1.1")
 end
 
 @testset "reduce hcat" begin
@@ -27,10 +27,10 @@ end
 end
 
 @testset "vcat" begin
-    test_rrule(vcat, randn(2, 4), randn(1, 4), randn(3, 4))
-    test_rrule(vcat, rand(), rand())
-    test_rrule(vcat, rand(), rand(3), rand(3,1,1))
-    test_rrule(vcat, rand(3,1,2), rand(4,1,2))
+    test_rrule(vcat, randn(2, 4), randn(1, 4), randn(3, 4); check_inferred=VERSION>v"1.1")
+    test_rrule(vcat, rand(), rand(); check_inferred=VERSION>v"1.1")
+    test_rrule(vcat, rand(), rand(3), rand(3,1,1); check_inferred=VERSION>v"1.1")
+    test_rrule(vcat, rand(3,1,2), rand(4,1,2); check_inferred=VERSION>v"1.1")
 end
 
 @testset "reduce vcat" begin
@@ -45,17 +45,17 @@ end
 end
 
 @testset "cat" begin
-    test_rrule(cat, rand(2, 4), rand(1, 4); fkwargs=(dims=1,))
-    test_rrule(cat, rand(2, 4), rand(2); fkwargs=(dims=Val(2),))
-    test_rrule(cat, rand(), rand(2, 3); fkwargs=(dims=[1,2],))
+    test_rrule(cat, rand(2, 4), rand(1, 4); fkwargs=(dims=1,), check_inferred=VERSION>v"1.1")
+    test_rrule(cat, rand(2, 4), rand(2); fkwargs=(dims=Val(2),), check_inferred=VERSION>v"1.1")
+    test_rrule(cat, rand(), rand(2, 3); fkwargs=(dims=[1,2],), check_inferred=VERSION>v"1.1")
     test_rrule(cat, rand(1), rand(3, 2, 1); fkwargs=(dims=(1,2),), check_inferred=false) # infers Tuple{Zero, Vector{Float64}, Any}
 end
 
 @testset "hvcat" begin
-    test_rrule(hvcat, 2 ⊢ NoTangent(), rand(ComplexF64, 6)...)
-    test_rrule(hvcat, (2, 1) ⊢ NoTangent(), rand(), rand(1,1), rand(2,2))
-    test_rrule(hvcat, 1 ⊢ NoTangent(), rand(3)' ⊢ rand(1,3), transpose(rand(3)) ⊢ rand(1,3))
-    test_rrule(hvcat, 1 ⊢ NoTangent(), rand(0,3), rand(2,3), rand(1,3,1))
+    test_rrule(hvcat, 2 ⊢ NoTangent(), rand(ComplexF64, 6)...; check_inferred=VERSION>v"1.1")
+    test_rrule(hvcat, (2, 1) ⊢ NoTangent(), rand(), rand(1,1), rand(2,2); check_inferred=VERSION>v"1.1")
+    test_rrule(hvcat, 1 ⊢ NoTangent(), rand(3)' ⊢ rand(1,3), transpose(rand(3)) ⊢ rand(1,3); check_inferred=VERSION>v"1.1")
+    test_rrule(hvcat, 1 ⊢ NoTangent(), rand(0,3), rand(2,3), rand(1,3,1); check_inferred=VERSION>v"1.1")
 end
 
 @testset "fill" begin
