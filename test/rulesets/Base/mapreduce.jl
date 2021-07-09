a20fd0905708be51a02280f251cee31dbc2510de
Alex Robson <alex.robson@invenialabs.co.uk>
2021-07-09 20:39:08 +0100

    Add in some tests

--- test/rulesets/Base/mapreduce.jl
+++ test/rulesets/Base/mapreduce.jl
@@ -1,115 +1,167 @@
 @testset "Maps and Reductions" begin
-    @testset "sum" begin
-        sizes = (3, 4, 7)
-        @testset "dims = $dims" for dims in (:, 1)
-            @testset "Array{$N, $T}" for N in eachindex(sizes), T in (Float64, ComplexF64)
-                x = randn(T, sizes[1:N]...)
-                test_frule(sum, x; fkwargs=(;dims=dims))
-                test_rrule(sum, x; fkwargs=(;dims=dims))
-            end
-        end
-    end
+    # @testset "sum" begin
+    #     sizes = (3, 4, 7)
+    #     @testset "dims = $dims" for dims in (:, 1)
+    #         @testset "Array{$N, $T}" for N in eachindex(sizes), T in (Float64, ComplexF64)
+    #             x = randn(T, sizes[1:N]...)
+    #             test_frule(sum, x; fkwargs=(;dims=dims))
+    #             test_rrule(sum, x; fkwargs=(;dims=dims))
+    #         end
+    #     end
+    # end
 
-    @testset "sum abs2" begin
-        sizes = (3, 4, 7)
-        @testset "dims = $dims" for dims in (:, 1)
-            @testset "Array{$N, $T}" for N in eachindex(sizes), T in (Float64, ComplexF64)
-                x = randn(T, sizes[1:N]...)
-                test_frule(sum, abs2, x; fkwargs=(;dims=dims))
-                test_rrule(sum, abs2 ⊢ NoTangent(), x; fkwargs=(;dims=dims))
-            end
-        end
-    end  # sum abs2
+    # @testset "sum abs2" begin
+    #     sizes = (3, 4, 7)
+    #     @testset "dims = $dims" for dims in (:, 1)
+    #         @testset "Array{$N, $T}" for N in eachindex(sizes), T in (Float64, ComplexF64)
+    #             x = randn(T, sizes[1:N]...)
+    #             test_frule(sum, abs2, x; fkwargs=(;dims=dims))
+    #             test_rrule(sum, abs2 ⊢ NoTangent(), x; fkwargs=(;dims=dims))
+    #         end
+    #     end
+    # end  # sum abs2
 
-    @testset "sum(f, xs)" begin
-        # This calls back into AD
-        test_rrule(sum, abs, [-4.0, 2.0, 2.0])
-        test_rrule(sum, Multiplier(2.0), [2.0, 4.0, 8.0])
+    # @testset "sum(f, xs)" begin
+    #     # This calls back into AD
+    #     test_rrule(sum, abs, [-4.0, 2.0, 2.0])
+    #     test_rrule(sum, Multiplier(2.0), [2.0, 4.0, 8.0])
 
-        test_rrule(sum, sum, [[2.0, 4.0], [4.0,1.9]])  # array of arrays
+    #     test_rrule(sum, sum, [[2.0, 4.0], [4.0,1.9]])  # array of arrays
         
-        # dims kwarg
-        test_rrule(sum, abs, [-2.0 4.0; 5.0 1.9]; fkwargs=(;dims=1))
-        test_rrule(sum, abs, [-2.0 4.0; 5.0 1.9]; fkwargs=(;dims=2))
+    #     # dims kwarg
+    #     test_rrule(sum, abs, [-2.0 4.0; 5.0 1.9]; fkwargs=(;dims=1))
+    #     test_rrule(sum, abs, [-2.0 4.0; 5.0 1.9]; fkwargs=(;dims=2))
 
-        test_rrule(sum, abs, @SVector[1.0, -3.0])
+    #     test_rrule(sum, abs, @SVector[1.0, -3.0])
 
-        # covectors
-        x = [-4.0 2.0; 2.0 -1.0]
-        test_rrule(sum, inv, x[1, :]')
-        test_rrule(sum, inv, x[1:1, :]')
-        test_rrule(sum, inv, transpose(view(x, 1, :)))
+    #     # covectors
+    #     x = [-4.0 2.0; 2.0 -1.0]
+    #     test_rrule(sum, inv, x[1, :]')
+    #     test_rrule(sum, inv, x[1:1, :]')
+    #     test_rrule(sum, inv, transpose(view(x, 1, :)))
 
-        # Make sure we preserve type for StaticArrays
-        ADviaRuleConfig = ChainRulesTestUtils.ADviaRuleConfig
-        _, pb = rrule(ADviaRuleConfig(), sum, abs, @SVector[1.0, -3.0])
-        @test pb(1.0) isa Tuple{NoTangent, NoTangent, SVector{2, Float64}}
+    #     # Make sure we preserve type for StaticArrays
+    #     ADviaRuleConfig = ChainRulesTestUtils.ADviaRuleConfig
+    #     _, pb = rrule(ADviaRuleConfig(), sum, abs, @SVector[1.0, -3.0])
+    #     @test pb(1.0) isa Tuple{NoTangent, NoTangent, SVector{2, Float64}}
       
 
-        # For structured sparse matrixes we screw it up, getting dense back
-        # see https://github.com/JuliaDiff/ChainRules.jl/issues/232 etc
-        _, pb = rrule(ADviaRuleConfig(), sum, abs, Diagonal([1.0, -3.0]))
-        @test_broken pb(1.0)[3] isa Diagonal
+    #     # For structured sparse matrixes we screw it up, getting dense back
+    #     # see https://github.com/JuliaDiff/ChainRules.jl/issues/232 etc
+    #     _, pb = rrule(ADviaRuleConfig(), sum, abs, Diagonal([1.0, -3.0]))
+    #     @test_broken pb(1.0)[3] isa Diagonal
+    # end
+
+    # @testset "prod" begin
+    #     @testset "Array{$T}" for T in [Float64, ComplexF64]
+    #         @testset "size = $sz, dims = $dims" for (sz, dims) in [
+    #             ((12,), :), ((12,), 1),
+    #             ((3,4), 1), ((3,4), 2), ((3,4), :), ((3,4), [1,2]),
+    #             ((3,4,1), 1), ((3,2,2), 3), ((3,2,2), 2:3),
+    #             ]
+    #             x = randn(T, sz)
+    #             test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
+    #             x[1] = 0
+    #             test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
+    #             x[5] = 0
+    #             test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
+    #             x[3] = x[7] = 0  # two zeros along some slice, for any dims
+    #             test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
+
+    #             if ndims(x) == 3
+    #                 xp = PermutedDimsArray(x, (3,2,1))  # not a StridedArray
+    #                 xpdot, xpbar = permutedims(rand(T, sz), (3,2,1)), permutedims(rand(T, sz), (3,2,1))
+    #                 test_rrule(prod, xp ⊢ xpbar; fkwargs=(dims=dims,), check_inferred=true)
+    #             end
+    #         end
+
+    #         @testset "structured wrappers" begin
+    #             # Adjoint -- like PermutedDimsArray this may actually be used
+    #             xa = adjoint(rand(T,4,4))
+    #             test_rrule(prod, xa ⊢ rand(T,4,4))
+    #             test_rrule(prod, xa ⊢ rand(T,4,4), fkwargs=(dims=2,))
+    #             @test unthunk(rrule(prod, adjoint(rand(T,3,3)))[2](1.0)[2]) isa Matrix
+    #             @test unthunk(rrule(prod, adjoint(rand(T,3,3)), dims=1)[2](ones(1,3))[2]) isa Matrix
+
+    #             # Diagonal -- a stupid thing to do, product of zeros! Shouldn't be an error though:
+    #             @test iszero(unthunk(rrule(prod, Diagonal(rand(T,3)))[2](1.0)[2]))
+    #             @test iszero(unthunk(rrule(prod, Diagonal(rand(T,3)), dims=1)[2](ones(1,3))[2]))
+    #             # does a division for the complex case, so is not necessarily exact
+    #             @test isapprox(
+    #                 unthunk(rrule(prod, Diagonal(rand(T,1)))[2](1.0)[2]), # 1x1 sparse matrix
+    #                 hcat(1);
+    #                 rtol=T <: Complex ? 2eps() : 0.0,
+    #             )
+    #             @test unthunk(rrule(prod, Diagonal(ones(T,2)), dims=1)[2](ones(1,2))[2]) == [0 1; 1 0]
+
+    #             # Triangular -- almost equally stupud
+    #             @test iszero(unthunk(rrule(prod, UpperTriangular(rand(T,3,3)))[2](1.0)[2]))
+    #             @test unthunk(rrule(prod, UpperTriangular(ones(T,2,2)))[2](1.0)[2]) == [0 0; 1 0]
+
+    #             # Symmetric -- at least this doesn't have zeros, still an unlikely combination
+    #             xs = Symmetric(rand(T,4,4))
+    #             @test_skip test_rrule(prod, xs ⊢ rand(T,4,4))
+    #             @test_skip test_rrule(prod, xs ⊢ rand(T,4,4), fkwargs=(dims=2,))
+    #             @test unthunk(rrule(prod, Symmetric(T[1 2; -333 4]))[2](1.0)[2]) == [16 8; 8 4]
+    #         end
+    #     end
+    #     @testset "Array{Float32}, no zero entries" begin
+    #         v = [1f-5, 1f-10, 1f-15, 1f-20]
+    #         @test prod(v) == 0
+    #         @test unthunk(rrule(prod, v)[2](1f0)[2]) == zeros(4)
+    #         test_rrule(prod, v)
+    #     end
+    # end # prod
+
+    @testset "Map" begin
+
+        function _test_mixed_map(f, xs)
+
+            primal = map(f, xs)
+
+            # Test there is an `Xrule` for f(x_i)
+            # This isn't really a test, it's just a check that we 
+            # have something to do the AD with. 
+            ∂self = NoTangent()
+            @assert ChainRules.rrule(f, first(xs)) != nothing
+            @assert ChainRules.frule((∂self, one(first(xs))), f, first(xs)) != nothing
+
+            y1, pb1 = ChainRules.rrule(NoForwardModeAD(), map, f, xs)
+            y2, pb2 = ChainRules.rrule(MixedModeAD(), map, f, xs)
+
+            # Test that the tangents are the same
+            r = rand_tangent(primal);
+            @test all(pb1(r)[3] .≈ pb2(r)[3])
+
+            @testset "$config" for config in (NoForwardModeAD(), MixedModeAD())
+                test_rrule(config, map, f, xs; check_inferred = false)
+            end
+        end
+        _psd() = (r = rand(20,20); return r' * r)
+
+        @testset "f: ℛ -> ℛ" begin
+            @testset "$f" for f in (sinpi, cospi)
+                f = sinpi
+                xs = collect(range(0, stop = 20, length = 101))
+                _test_mixed_map(f, xs)
+            end
+        end
+
+        @testset "f: ℛ² -> ℛ²" begin
+            @testset "$f" for f in (inv,)
+                Random.seed!(1)
+                xs = collect(_psd() for _ in 1:10)
+                _test_mixed_map(f, xs)
+            end
+        end
+
+        @testset "f: ℛ² -> ℛ" begin
+            @testset "$f" for f in (det,tr)
+                Random.seed!(1)
+                xs = collect(_psd() for _ in 1:10)
+                _test_mixed_map(f, xs)
+            end
+        end
     end
 
-    @testset "prod" begin
-        @testset "Array{$T}" for T in [Float64, ComplexF64]
-            @testset "size = $sz, dims = $dims" for (sz, dims) in [
-                ((12,), :), ((12,), 1),
-                ((3,4), 1), ((3,4), 2), ((3,4), :), ((3,4), [1,2]),
-                ((3,4,1), 1), ((3,2,2), 3), ((3,2,2), 2:3),
-                ]
-                x = randn(T, sz)
-                test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
-                x[1] = 0
-                test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
-                x[5] = 0
-                test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
-                x[3] = x[7] = 0  # two zeros along some slice, for any dims
-                test_rrule(prod, x; fkwargs=(dims=dims,), check_inferred=true)
-
-                if ndims(x) == 3
-                    xp = PermutedDimsArray(x, (3,2,1))  # not a StridedArray
-                    xpdot, xpbar = permutedims(rand(T, sz), (3,2,1)), permutedims(rand(T, sz), (3,2,1))
-                    test_rrule(prod, xp ⊢ xpbar; fkwargs=(dims=dims,), check_inferred=true)
-                end
-            end
-
-            @testset "structured wrappers" begin
-                # Adjoint -- like PermutedDimsArray this may actually be used
-                xa = adjoint(rand(T,4,4))
-                test_rrule(prod, xa ⊢ rand(T,4,4))
-                test_rrule(prod, xa ⊢ rand(T,4,4), fkwargs=(dims=2,))
-                @test unthunk(rrule(prod, adjoint(rand(T,3,3)))[2](1.0)[2]) isa Matrix
-                @test unthunk(rrule(prod, adjoint(rand(T,3,3)), dims=1)[2](ones(1,3))[2]) isa Matrix
-
-                # Diagonal -- a stupid thing to do, product of zeros! Shouldn't be an error though:
-                @test iszero(unthunk(rrule(prod, Diagonal(rand(T,3)))[2](1.0)[2]))
-                @test iszero(unthunk(rrule(prod, Diagonal(rand(T,3)), dims=1)[2](ones(1,3))[2]))
-                # does a division for the complex case, so is not necessarily exact
-                @test isapprox(
-                    unthunk(rrule(prod, Diagonal(rand(T,1)))[2](1.0)[2]), # 1x1 sparse matrix
-                    hcat(1);
-                    rtol=T <: Complex ? 2eps() : 0.0,
-                )
-                @test unthunk(rrule(prod, Diagonal(ones(T,2)), dims=1)[2](ones(1,2))[2]) == [0 1; 1 0]
-
-                # Triangular -- almost equally stupud
-                @test iszero(unthunk(rrule(prod, UpperTriangular(rand(T,3,3)))[2](1.0)[2]))
-                @test unthunk(rrule(prod, UpperTriangular(ones(T,2,2)))[2](1.0)[2]) == [0 0; 1 0]
-
-                # Symmetric -- at least this doesn't have zeros, still an unlikely combination
-                xs = Symmetric(rand(T,4,4))
-                @test_skip test_rrule(prod, xs ⊢ rand(T,4,4))
-                @test_skip test_rrule(prod, xs ⊢ rand(T,4,4), fkwargs=(dims=2,))
-                @test unthunk(rrule(prod, Symmetric(T[1 2; -333 4]))[2](1.0)[2]) == [16 8; 8 4]
-            end
-        end
-        @testset "Array{Float32}, no zero entries" begin
-            v = [1f-5, 1f-10, 1f-15, 1f-20]
-            @test prod(v) == 0
-            @test unthunk(rrule(prod, v)[2](1f0)[2]) == zeros(4)
-            test_rrule(prod, v)
-        end
-    end # prod
 end
