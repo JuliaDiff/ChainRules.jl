0b390286dea22ed16671abacf6a249a360cbc74c
Keno Fischer <keno@juliacomputing.com>
2021-07-28 22:02:12 -0400

    Add more non-differentiable builtins
    As reported in https://github.com/JuliaDiff/Diffractor.jl/issues/19

--- src/rulesets/Core/core.jl
+++ src/rulesets/Core/core.jl
@@ -4,5 +4,8 @@
 @non_differentiable Core.show(::Any)
 @non_differentiable Core.show(::Any, ::Any)
 
+@non_differentiable Core.isdefined(::Any, ::Any)
+@non_differentiable Core.:(<:)(::Any, ::Any)
+
 @non_differentiable Core.apply_type(::Any, ::Any)
 @non_differentiable Core.typeof(::Any)
