8281539c7f32e4db85ea401e02b7198fbd5ba780
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-08-05 08:42:11 -0400

    unique, take 1

--- src/rulesets/Base/sort.jl
+++ src/rulesets/Base/sort.jl
@@ -1,3 +1,7 @@
+#####
+##### `sort`
+#####
+
 function rrule(::typeof(partialsort), xs::AbstractVector, k::Union{Integer,OrdinalRange}; kwargs...)
     inds = partialsortperm(xs, k; kwargs...)
     ys = xs[inds]
@@ -33,3 +37,53 @@ function rrule(::typeof(sort), xs::AbstractVector; kwargs...)
     end
     return ys, sort_pullback
 end
+
+#####
+##### `unique`
+#####
+
+function rrule(::typeof(unique), x::AbstractArray{<:Number}; dims=:)
+    axes_x = axes(x)
+    project = ProjectTo(x)
+    y = unique(x; dims=dims)  # accepts only dims=: or dims::Integer
+    if dims isa Colon
+        xs, ys = vec(x), y
+    else
+        xs, ys = collect(eachslice(x; dims=dims)), collect(eachslice(y; dims=dims))
+    end
+    mask = isequal.(permutedims(ys), xs)  # unique([0.0, -0.0, NaN, NaN])
+    mask .= (mask .== cumsum(mask, dims=1) .== true)
+    keep = map(I -> I[1], findall(mask))
+    function unique_pullback(dy_raw)
+        dy = unthunk(dy_raw)
+        if dims isa Colon
+            # The function `_zerolike_writeat` is defined near `maximum`, allows
+            # second derivatives. Should perhaps eventually be shared with `getindex`.
+            dx = reshape(_zerolike_writeat(vec(x), vec(dy), (), keep), axes_x)
+        else
+            inds = ntuple(d -> d==dims ? keep : (:), length(axes_x))
+            dx = _zerolike_writeat(x, dy, (), inds...)
+        end            
+        return (NoTangent(), project(dx))
+    end
+    return y, unique_pullback
+end
+
+function _zerolike_writeat(x, dy, dims, ind...)
+    # It's unfortunate to close over `x`, but `similar(typeof(x), axes(x))` doesn't 
+    # allow `eltype(dy)`, nor does it work for many structured matrices.
+    dx = fill!(similar(x, eltype(dy), axes(x)), false)
+    view(dx, ind...) .= dy  # possibly 0-dim view, allows dy::Number and dy::Array, and dx::CuArray
+    dx
+end
+
+#=
+
+rrule(unique, [1,1,2,3])[2]([10,20,30]) == (NoTangent(), [10, 0, 20, 30])
+rrule(unique, [1 2; 1 4])[2]([10,20,30]) == (NoTangent(), [10 20; 0 30])
+
+rrule(unique, [1 2 1 2; 1 2 1 4], dims=2)[2]([10 20 30; 40 50 60])[2] == [10 20 0 30; 40 50 0 60]
+
+rrule(unique, Diagonal([1,2,3]))[2]([10 20 30 40])[2] == [10.0 0.0 0.0; 0.0 30.0 0.0; 0.0 0.0 40.0]
+
+=#
