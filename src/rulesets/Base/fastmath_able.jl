6da152a5d8b50bc1413000ccbb6059f34cd61993
rheintzmann <heintzmann@gmail.com>
2021-08-11 17:09:32 +0200

    fixed tuple for sincospi rule

--- src/rulesets/Base/fastmath_able.jl
+++ src/rulesets/Base/fastmath_able.jl
@@ -49,7 +49,7 @@ let
         @scalar_rule atan(y, x) @setup(u = x ^ 2 + y ^ 2) (x / u, -y / u)
         @scalar_rule sincos(x) @setup((sinx, cosx) = Ω) cosx -sinx
         # the position of the minus sign below warrants the correct type for π  
-        @scalar_rule sincospi(x) @setup((sinpix, cospix) = Ω) (π * cospix, π * (-sinpix))
+        @scalar_rule sincospi(x) @setup((sinpix, cospix) = Ω) (π * cospix)  (π * (-sinpix))
 
         # exponents
         @scalar_rule cbrt(x) inv(3 * Ω ^ 2)
