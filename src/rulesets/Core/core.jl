1b61c2466acb66518bada9a93013f802e22c3d8c
Andrei Zhabinski <faithlessfriend@gmail.com>
2021-08-16 00:58:46 +0300

    Add rrule for Core._apply_iterate

--- src/rulesets/Core/core.jl
+++ src/rulesets/Core/core.jl
@@ -32,3 +32,34 @@ function rrule(::typeof(ifelse), c, a::Number, b::Number)
     ifelse_pullback(Δ) = (NoTangent(), NoTangent(), ifelse(c, Δ, zero(Δ)), ifelse(c, zero(Δ), Δ))
     return ifelse(c, a, b), ifelse_pullback
 end
+
+function rrule(
+    ::typeof(Core._apply_iterate), ::typeof(iterate), f::F, args...) where F
+    # flatten nested arguments
+    flat = []
+    for a in args
+        push!(flat, a...)
+    end
+    # apply rrule of the function on the flat arguments
+    y, pb = rrule(f, flat...)
+    sizes = map(length, args)
+    function _apply_iterate_pullback(dy)
+        flat_dargs = pb(dy)
+        df = flat_dargs[1]
+        # group derivatives to tuples of the same sizes as arguments
+        dargs = []
+        j = 2
+        for i=1:length(args)
+            darg_val = flat_dargs[j:j + sizes[i] - 1]
+            if length(darg_val) == 1 && darg_val[1] isa NoTangent
+                push!(dargs, darg_val[1])
+            else
+                darg = Tangent{typeof(darg_val)}(darg_val...)
+                push!(dargs, darg)
+            end
+            j = j + sizes[i]
+        end
+        return NoTangent(), NoTangent(), df, dargs...
+    end
+    return y, _apply_iterate_pullback
+end
--- test/rulesets/Core/core.jl
+++ test/rulesets/Core/core.jl
@@ -10,3 +10,10 @@ end
     test_rrule(ifelse, true, [1.1], [2.0]; check_inferred=false)
     test_frule(ifelse, false, [1.1], [2.0]; check_inferred=false)
 end
+
+@testset "_apply_iterate" begin
+    test_rrule(
+        Core._apply_iterate, iterate, reshape, (rand(3, 4),), (12,);
+        check_inferred=false
+    )
+end
