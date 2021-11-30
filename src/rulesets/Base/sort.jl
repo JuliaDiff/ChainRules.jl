ce78d3d3e8aaf6303e1aa7085fdbdfc2d36d1b64
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2021-11-30 17:05:13 -0500

    Rules for `sortslices`, `unique` (#546)
    * unique, take 1
    
    * add shortcut, and tests
    
    * sortslices, too
    
    * fixup
    
    * Apply 3 suggestions
    
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>
    
    * comment
    
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>
    
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>

--- src/rulesets/Base/sort.jl
+++ src/rulesets/Base/sort.jl
@@ -1,3 +1,7 @@
+#####
+##### `sort`
+#####
+
 function rrule(::typeof(partialsort), xs::AbstractVector, k::Union{Integer,OrdinalRange}; kwargs...)
     inds = partialsortperm(xs, k; kwargs...)
     ys = xs[inds]
@@ -32,4 +36,56 @@ function rrule(::typeof(sort), xs::AbstractVector; kwargs...)
         return NoTangent(), Δxs
     end
     return ys, sort_pullback
+end
+
+#####
+##### `sortslices`
+#####
+
+function rrule(::typeof(sortslices), x::AbstractArray; dims::Integer, kw...)
+    p = sortperm(collect(eachslice(x; dims=dims)); kw...)
+    inds = ntuple(d -> d == dims ? p : (:), ndims(x))
+    function sortslices_pullback(dy)
+        # No actual need to zero this, and if you didn't, then you could widen eltype
+        # Also, you could use similar(dy) here not x, same size?
+        dx = _zerolike_writeat(x, unthunk(dy), (), inds...)
+        return (NoTangent(), ProjectTo(x)(dx))
+    end
+    return x[inds...], sortslices_pullback
+end
+
+#####
+##### `unique`
+#####
+
+function rrule(::typeof(unique), x::AbstractArray{<:Number}; dims=:)
+    axes_x = axes(x)
+    y = unique(x; dims=dims)  # accepts only dims=: or dims::Integer
+    function unique_pullback(dy_raw)
+        dy = unthunk(dy_raw)
+        if length(x) == length(y)
+            # Short-circuit for the case of all unique, since `mask` is fairly expensive:
+            dx = reshape(dy, axes_x)
+            return (NoTangent(), ProjectTo(x)(dx))
+        end
+
+        if dims isa Colon
+            xs, ys = vec(x), y
+        else
+            xs, ys = collect(eachslice(x; dims=dims)), collect(eachslice(y; dims=dims))
+        end
+        mask = isequal.(permutedims(ys), xs)  # unique([0.0, -0.0, NaN, NaN])
+        mask .= (mask .== cumsum(mask, dims=1) .== true)  # this implements  findfirst(mask; dims=1)
+        keep = map(I -> I[1], findall(mask))
+        if dims isa Colon
+            # The function `_zerolike_writeat` allows second derivatives.
+            # Should perhaps eventually be shared with `getindex`.
+            dx = reshape(_zerolike_writeat(vec(x), vec(dy), (), keep), axes_x)
+        else
+            inds = ntuple(d -> d==dims ? keep : (:), length(axes_x))
+            dx = _zerolike_writeat(x, dy, (), inds...)
+        end
+        return (NoTangent(), ProjectTo(x)(dx))
+    end
+    return y, unique_pullback
 end
