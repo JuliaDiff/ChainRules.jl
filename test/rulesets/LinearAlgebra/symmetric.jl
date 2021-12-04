acdb4c7020c2fa1ba8f7e0636dad2ef9acd6effa
Lyndon White <oxinabox@ucc.asn.au>
2021-12-04 09:13:00 -0600

    type
    Co-authored-by: Michael Abbott <32575566+mcabbott@users.noreply.github.com>

--- test/rulesets/LinearAlgebra/symmetric.jl
+++ test/rulesets/LinearAlgebra/symmetric.jl
@@ -307,7 +307,7 @@
                 λ = if f in (acos, asin, atanh)
                     # generate random number between -1 and 0.9. This would be between
                     # -1 and 1 but we want to avoid domain errors from numerical error
-                    1.9 .* rand(real(T), n) .- 1
+                    T(1.9) .* rand(real(T), n) .- 1
                 elseif f in (log, sqrt)
                     abs.(randn(real(T), n))
                 elseif f === acosh
