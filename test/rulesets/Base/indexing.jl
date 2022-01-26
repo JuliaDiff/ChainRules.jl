3355ae7306f7d624cf9b0c55717cade5ce7865d7
Michael Abbott <32575566+mcabbott@users.noreply.github.com>
2022-01-26 15:06:09 -0500

    Rules for `eachslice` (#561)
    * rules for eachslice
    
    * test only 1.6+
    
    * move methods, use projection, allow 2nd derivatives
    
    * fixup
    
    * avoid limitations of ComposedFunction on 1.6
    
    * fixup rebase, and bump version
    
    * change to at-assert
    
    * Apply 5 suggestions
    
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>
    
    Co-authored-by: Lyndon White <oxinabox@ucc.asn.au>

--- test/rulesets/Base/indexing.jl
+++ test/rulesets/Base/indexing.jl
@@ -68,3 +68,34 @@ end
     test_frule(setindex!, rand(3, 4), rand(), 1, 2)
     test_frule(setindex!, rand(3, 4), [1,10,100.0], :, 3)
 end
+
+@testset "eachslice" begin
+    # Testing eachrow not collect∘eachrow leads to errors, e.g.
+    # test_rrule: eachrow on Vector{Float64}: Error During Test at /Users/me/.julia/packages/ChainRulesTestUtils/8dFTY/src/testers.jl:195
+    #   Got exception outside of a @test
+    #   DimensionMismatch("second dimension of A, 6, does not match length of x, 5")
+    # Probably similar to https://github.com/JuliaDiff/ChainRulesTestUtils.jl/issues/234 (about Broadcasted not Generator)
+
+    test_rrule(collect∘eachrow, rand(5))
+    test_rrule(collect∘eachrow, rand(3, 4))
+
+    test_rrule(collect∘eachcol, rand(3, 4))
+    @test_skip test_rrule(collect∘eachcol, Diagonal(rand(5)))  # works locally!
+
+    if VERSION >= v"1.7"
+        # On 1.6, ComposedFunction doesn't take keywords. Only affects this testing strategy, not real use.
+        test_rrule(collect∘eachslice, rand(3, 4, 5); fkwargs = (; dims = 3))
+        test_rrule(collect∘eachslice, rand(3, 4, 5); fkwargs = (; dims = (2,)))
+    end
+
+    # Make sure pulling back an array that mixes some AbstractZeros in works right
+    _, back = rrule(eachcol, rand(3, 4))
+    @test back([1:3, ZeroTangent(), 7:9, NoTangent()]) == (NoTangent(), [1 0 7 0; 2 0 8 0; 3 0 9 0])
+    @test back([1:3, ZeroTangent(), 7:9, NoTangent()])[2] isa Matrix{Float64}
+    @test back([ZeroTangent(), ZeroTangent(), NoTangent(), NoTangent()]) == (NoTangent(), [0 0 0 0; 0 0 0 0; 0 0 0 0])
+
+    # Second derivative rule
+    test_rrule(ChainRules.∇eachslice, [rand(4) for _ in 1:3], rand(3, 4), Val(1))
+    test_rrule(ChainRules.∇eachslice, [rand(3) for _ in 1:4], rand(3, 4), Val(2))
+    test_rrule(ChainRules.∇eachslice, [rand(2, 3) for _ in 1:4], rand(2, 3, 4), Val(3), check_inferred=false)
+end
