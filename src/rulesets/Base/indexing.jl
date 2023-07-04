05ebb38f2e09a6c348d2e84d1f04af893069e8d5
Keno Fischer <keno@juliacomputing.com>
2023-07-04 03:52:00 -0400

    Remove frule for getindex(::Tuple, i) (#680)
    * Remove frule for getindex(::Tuple, i)
    
    Having this chain rule is sub-optimal, because it prevents early-SROA
    in Diffractor-like systems that would like to perform some optimizations
    before applying AD (but can't do any optimization on functions that
    have custom rules). By letting it go down to the `getfield`, regular
    SROA can apply. Any AD system should handle `getfield` anyway, so
    I don't think there's a strong reason to have this.
    
    Similar reasoning applies to the reverse rules also, but they
    aren't currently actively causing me problems, so this PR only
    removes the frule, since I don't think many other packages are
    using them. We can revisit the rrules later.
    
    * Also remove the rules for first/tail
    
    For similar reasons as getindex, having a rule for first/tail is
    suboptimal because it supresses early SROA. Tail is particularly
    problematic, because it is used in the implementation of the
    ```
    x, y... = abc
    ```
    syntax, of which users expect early elimination.
    
    * add getfield rule and remove tests for deleted rules
    
    ---------
    
    Co-authored-by: Oscar Smith <oscardssmith@gmail.com>

--- src/rulesets/Base/indexing.jl
+++ src/rulesets/Base/indexing.jl
@@ -1,14 +1,6 @@
-#####
-##### getindex(::Tuple)
-#####
-
-function frule((_, ẋ), ::typeof(getindex), x::Tuple, i::Integer)
-    return x[i], ẋ[i]
-end
-
-function frule((_, ẋ), ::typeof(getindex), x::Tuple, i)
-    y = x[i]
-    return y, Tangent{typeof(y)}(ẋ[i]...)
+# Int rather than Int64/Integer is intentional
+function frule((_, ẋ), ::typeof(getfield), x::Tuple, i::Int)
+    return x.i, ẋ.i
 end
 
 "for a given tuple type, returns a Val{N} where N is the length of the tuple"
@@ -77,7 +69,7 @@ end
 """
     ∇getindex(x, dy, inds...)
 
-For the `rrule` of `y = x[inds...]`, this function is roughly 
+For the `rrule` of `y = x[inds...]`, this function is roughly
 `setindex(zero(x), dy, inds...)`, returning the array `dx`.
 Differentiable. Includes `ProjectTo(x)(dx)`.
 """
@@ -189,29 +181,6 @@ function ∇getindex!(dx::AbstractGPUArray, dy, inds...)
     view(dx_cpu, adapt(Array, inds)...) .+= adapt(Array, dy)
     copyto!(dx, dx_cpu)
     return dx
-end
-
-#####
-##### first, tail
-#####
-
-function frule((_, ẋ), ::typeof(first), x::Tuple)
-    return first(x), first(ẋ)
-end
-
-function rrule(::typeof(first), x::T) where {T<:Tuple}
-    first_back(dy) = (NoTangent(), Tangent{T}(ntuple(j -> j == 1 ? dy : NoTangent(), _tuple_N(T))...))
-    return first(x), first_back
-end
-
-function frule((_, ẋ), ::typeof(Base.tail), x::Tuple)
-    y = Base.tail(x)
-    return y, Tangent{typeof(y)}(Base.tail(ẋ)...)
-end
-
-function rrule(::typeof(Base.tail), x::T) where {T<:Tuple}
-    tail_pullback(dy) = (NoTangent(), Tangent{T}(NoTangent(), dy...))
-    return Base.tail(x), tail_pullback
 end
 
 #####
