abf9e3757eb3ead01a519fdeeb6b32eb285d87f3
Frames White <me@oxinabox.net>
2024-02-12 14:33:36 +0800

    Add tricky cases

--- test/rulesets/Base/arraymath.jl
+++ test/rulesets/Base/arraymath.jl
@@ -216,5 +216,7 @@
         # rev
         @gpu test_rrule(+, randn(4, 4), randn(4, 4), randn(4, 4))
         @gpu test_rrule(+, randn(3), randn(3,1), randn(3,1,1))
+        test_rrule(+, randn(3,3), Diagonal(randn(3)), randn(3,3,1))
+        test_rrule(+, randn(3,3), Diagonal(randn(3)), Symmetric(randn(3,3)))
     end
 end
