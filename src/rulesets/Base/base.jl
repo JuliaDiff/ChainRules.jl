b54124051ef1d82155412e15c1b5aababfa61b92
Mike Boyle <moble@users.noreply.github.com>
2025-09-29 15:33:53 -0400

    Apply suggestions from code review
    Co-authored-by: David Müller-Widmann <devmotion@users.noreply.github.com>

--- src/rulesets/Base/base.jl
+++ src/rulesets/Base/base.jl
@@ -124,30 +124,28 @@ end
 function frule(
     (_, Δx, Δy, Δz, Δxs...),
     ::typeof(hypot),
-    x::T,
-    y::T,
-    z::T,
-    xs::Vararg{T, N}
-) where {T<:Union{Real,Complex}, N}
+    x::Union{Real,Complex},
+    y::Union{Real,Complex},
+    z::Union{Real,Complex},
+    xs::Union{Real,Complex}...,
+)
     Ω = hypot(x, y, z, xs...)
-    n = ifelse(iszero(Ω), one(Ω), Ω)
-    ∂Ωxyz = realdot(x, Δx) + realdot(y, Δy) + realdot(z, Δz)
-    ∂Ωxs = sum(realdot(xi, Δxi) for (xi, Δxi) in zip(xs, Δxs); init=zero(∂Ωxyz))
-    ∂Ω = (∂Ωxyz + ∂Ωxs) / n
+    n = ifelse(iszero(Ω), oneunit(Ω), Ω)
+    ∂Ω = sum(map(realdot, (x, y, z, xs...), (Δx, Δy, Δz, Δxs...))) / n
     return Ω, ∂Ω
 end
 
 function rrule(
     ::typeof(hypot),
-    x::T,
-    y::T,
-    z::T,
-    xs::Vararg{T, N}
-) where {T<:Union{Real,Complex}, N}
+    x::Union{Real,Complex},
+    y::Union{Real,Complex},
+    z::Union{Real,Complex},
+    xs::Union{Real,Complex}...,
+)
     Ω = hypot(x, y, z, xs...)
     function hypot_pullback(ΔΩ)
         c = real(ΔΩ) / ifelse(iszero(Ω), one(Ω), Ω)
-        return (NoTangent(), c * x, c * y, c * z, (c * xi for xi in xs)...)
+        return (NoTangent(), c * x, c * y, c * z, map(xi -> c * xi, xs)...)
     end
     return (Ω, hypot_pullback)
 end
